d08973974c0ef896410cd44ffdeaddcb647c931b
Thomas Draebing <thomas.draebing@sap.com>
2018-12-05 09:55:53 +0100

    Rewrite gerrit-init image to allow to handle other databases
    The gerrit-init container was only able to initialize a MySQL
    database. The provided bash scripts weren't easy to extend to support
    more databases. With Gerrit 2.16 a H2-database is usually enough, since
    the content does not change during runtime. Thus, at least this database
    should be supported.
    
    This change adapts the gerrit-init container to be able to support
    H2-databases. To do this, python scripts were used instead of bash,
    since it makes it easier to provide extensible code and since python
    possesses libraries to communicate with several databases, making it
    easier and more efficient to work with the databases.
    
    This change makes the Gerrit initialization in the helm charts more
    flexible. Also, using python and sqlalchemy to communicate with the
    database is a lot faster than using bash and the mysql client, greatly
    fasten up pod startup.
    
    Change-Id: Ic06de5ba6686b4c47365cbadb919c9a6ebfbd6b5

---
+++ container-images/gerrit-init/tools/git_config_parser.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+class GitConfigParser:
+
+  def __init__(self, config_path):
+    self.path = config_path
+
+  def _execute_shell_command_and_get_output_lines(self, command):
+    sub_process_run = subprocess.run(
+      command.split(),
+      stdout=subprocess.PIPE,
+      check=True,
+      universal_newlines=True)
+    return [line.strip() for line in sub_process_run.stdout.splitlines()]
+
+  def _get_value(self, key):
+    command = "git config -f %s --get %s" % (self.path, key)
+    return self._execute_shell_command_and_get_output_lines(command)
+
+  def get(self, key, default=None):
+    """
+    Returns value of given key in the configuration file. If the key appears
+    multiple times, the last value is returned.
+    """
+    try:
+      return self._get_value(key)[-1]
+    except subprocess.CalledProcessError:
+      return default
+
+  def get_boolean(self, key, default=False):
+    """
+    Returns boolean value of given key in the configuration file. If the key
+    appears multiple times, the last value is returned.
+    """
+    if not type(default)==bool:
+      raise TypeError("Default has to be a boolean.")
+
+    try:
+      value = self._get_value(key)[-1].lower()
+      if value == "true":
+        return True
+      elif value == "false":
+        return False
+      else:
+        raise TypeError("Value is not a boolean.")
+    except subprocess.CalledProcessError:
+      return default
