a8ebfe3dfdce9695b344b01f03c7d2b230037fe6
Thomas Draebing <thomas.draebing@sap.com>
2019-01-08 14:21:17 +0100

    Add structure test for base-container
    So far the containers created in this project weren't tested
    automatically.
    
    This change adds structure tests for the base-container making a start
    in the effort of implementing automated tests. The python package
    'pytest' is used to run the test and is meant to provide a framework to
    implement additional tests as well. Pipenv is used to ensure
    a consistent test environment.
    
    Change-Id: I04b1bdbc5b6360402b5a629c264c5c5c882f773f

---
+++ tests/container-images/base/test_container_structure_base.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+@pytest.fixture(scope="module")
+def container_run(request, docker_client, base_image):
+  print("Starting base-container...")
+  container_run = docker_client.containers.run(
+    image = base_image.id,
+    command = "tail -f /dev/null",
+    detach = True,
+    auto_remove = True
+  )
+
+  def stop_container():
+    print("Stopping base-container...")
+    container_run.stop(timeout=1)
+
+  request.addfinalizer(stop_container)
+
+  return container_run
+
+
+def test_base_contains_git(container_run):
+  exit_code, _ = container_run.exec_run(
+    "which git"
+  )
+  assert exit_code == 0
+
+def test_base_has_user_gerrit(container_run):
+  exit_code, _ = container_run.exec_run(
+    "id -u gerrit"
+  )
+  assert exit_code == 0
+
+def test_base_gerrit_not_root(container_run):
+  _, output = container_run.exec_run(
+    "id -u gerrit"
+  )
+  uid = int(output.strip().decode("utf-8"))
+  assert uid != 0
+
+def test_base_gerrit_no_root_permissions(container_run):
+  exit_code, _ = container_run.exec_run(
+    "su -c 'rm -rf /bin' gerrit"
+  )
+  assert exit_code > 0
