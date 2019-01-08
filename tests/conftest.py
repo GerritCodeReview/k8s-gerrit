348b840acc841c599a2924857f2d16f92e0cd8ba
Thomas Draebing <thomas.draebing@sap.com>
2019-01-08 14:21:17 +0100

    Add structure test for base-container
    So far the containers created in this project weren't tested
    automatically.
    
    This change adds structure tests for the base-container making a start
    in the effort of implementing automated tests. The python package
    'pytest' is used to run the test and is meant to provide a framework to
    implement additional tests as well.
    
    Change-Id: I04b1bdbc5b6360402b5a629c264c5c5c882f773f

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import docker
+import git
+import os
+import pytest
+
+@pytest.fixture(scope="session")
+def docker_client():
+  return docker.from_env()
+
+@pytest.fixture(scope="session")
+def repository_root():
+  git_repo = git.Repo('.', search_parent_directories=True)
+  return git_repo.git.rev_parse("--show-toplevel")
+
+@pytest.fixture(scope="session")
+def container_images(repository_root):
+  image_paths = dict()
+  for directory in os.listdir(os.path.join(repository_root, 'container-images')):
+    image_paths[directory] = os.path.join(
+      repository_root,
+      'container-images',
+      directory)
+  return image_paths
+
+@pytest.fixture(scope="session")
+def docker_build(docker_client):
+
+  def docker_build(image, tag):
+    build = docker_client.images.build(
+        path=image,
+        nocache=True,
+        rm=True,
+        tag=tag
+      )
+    return build[0]
+
+  return docker_build
+
+@pytest.fixture(scope="session")
+def base_image(container_images, docker_build):
+  return docker_build(container_images["base"], "base")
