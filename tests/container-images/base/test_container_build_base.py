e0a5517c344028471446118e9b0dcd023817ceda
Thomas Draebing <thomas.draebing@sap.com>
2019-01-15 16:33:58 +0100

    Add option to pytest to use container build cache
    When building container images during testing the build cache of Docker
    was disabled, enforcing a build from scratch. While this is a good
    behavior for tests, this also increased testing times. During
    development fast test times may be preferable.
    
    This change adds the '--build-cache'-flag to the 'pytest'-command, which
    activates usage of the build cache.
    
    Change-Id: Ifc9bcd4ca9b49ea1ad50199734de5dbe56f91b84

--- tests/container-images/base/test_container_build_base.py
+++ tests/container-images/base/test_container_build_base.py
@@ -14,8 +14,7 @@
 
 import pytest
 
-def test_build_base(base_image,
-    tag_of_cached_container):
+def test_build_base(base_image, tag_of_cached_container):
   if tag_of_cached_container:
     pytest.skip("Cached image used for testing. Build will not be tested.")
   assert base_image.id is not None
--- tests/container-images/git-gc/test_container_build_gitgc.py
+++ tests/container-images/git-gc/test_container_build_gitgc.py
@@ -14,8 +14,7 @@
 
 import pytest
 
-def test_build_gitgc(gitgc_image,
-    tag_of_cached_container):
+def test_build_gitgc(gitgc_image, tag_of_cached_container):
   if tag_of_cached_container:
     pytest.skip("Cached image used for testing. Build will not be tested.")
   assert gitgc_image.id is not None
