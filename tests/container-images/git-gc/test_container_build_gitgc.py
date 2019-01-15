79f8fcc0d2872ece8a5f7fadcb3e65c33e1ae445
Thomas Draebing <thomas.draebing@sap.com>
2019-01-15 15:37:14 +0100

    Allow to use existing container images
    The tests were always rebuilding the images from scratch. The rebuilding
    of the images took more time then the tests itself.
    
    This change adds the '--tag'-parameter to the pytest command, which
    takes a tag marking the existing container images. If this parameter is
    provided, the tests will use existing images with this tag and build
    only missing images. If the parameter is not provided, all images will
    be built as before.
    
    Change-Id: I262cf58129f23c898d2efb47d14c208ee1860763

--- tests/container-images/git-gc/test_container_build_gitgc.py
+++ tests/container-images/git-gc/test_container_build_gitgc.py
@@ -12,5 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-def test_build_gitgc(gitgc_image):
+import pytest
+
+def test_build_gitgc(gitgc_image,
+    tag_of_cached_container):
+  if tag_of_cached_container:
+    pytest.skip("Cached image used for testing. Build will not be tested.")
   assert gitgc_image.id is not None
