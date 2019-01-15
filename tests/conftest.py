d27cbf6d490dad59831661a292743254b2f06fcb
Thomas Draebing <thomas.draebing@sap.com>
2019-01-15 16:33:58 +0100

    Add option to pytest to use container build cache
    When building container images during testing the build cache of Docker
    was disabled, enforcing a build from scratch. While this is a good
    behavior for tests, this also increased testing times. During
    development fast test times may be preferable.
    
    This change adds the '--build-cache'-flag to the 'pytest'-command, which
    activates usage of the build cache.
    
    Change-Id: Ifc9bcd4ca9b49ea1ad50199734de5dbe56f91b84

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,10 @@ def pytest_addoption(parser):
     help="Tag of chached container images to test. Missing images will be built." +
          "(default: All container images will be built)"
   )
+  parser.addoption(
+    "--build-cache", action="store_true",
+    help="Whether to use the docker cache, when building container images."
+  )
 
 @pytest.fixture(scope="session")
 def tag_of_cached_container(request):
@@ -38,7 +42,7 @@ def container_images(repository_root):
   return image_paths
 
 @pytest.fixture(scope="session")
-def docker_build(docker_client, tag_of_cached_container):
+def docker_build(request, docker_client, tag_of_cached_container):
 
   def docker_build(image, name):
     image_name = "%s:latest" % name
@@ -52,9 +56,11 @@ def docker_build(docker_client, tag_of_cached_container):
       except:
         print("Image %s could not be loaded. Building it now." % image_name)
 
+    no_cache = not request.config.getoption("--build-cache")
+
     build = docker_client.images.build(
         path=image,
-        nocache=True,
+        nocache=no_cache,
         rm=True,
         tag=image_name
       )
