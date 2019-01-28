fc4ebc55d561aefa38e5083d658029a5249223fb
Thomas Draebing <thomas.draebing@sap.com>
2019-01-28 16:36:29 +0100

    Add a helper function for tests to wait for event
    A pattern used by many tests was to wait for a certain event happening
    in the container and to stay in a time limit. This was implemented in
    each test case using this pattern.
    
    This change adds a helper function available to all tests, that takes
    a function and a time limit. The function can return some output, which
    is forwarded to the test and has to return a boolean, indicating whether
    the event, for which the function tests has happened. The helper
    function will execute the given function as long as this function returns
    False or until the time limit is reached and will then return the
    functions output and a boolean indicating, whether the function returned
    True within the time limit.
    
    Change-Id: I3505e0f25028a81f84dc7e5b8f006e0d6a904ab4

--- tests/container-images/gerrit-master/test_container_integration_gerrit_master.py
+++ tests/container-images/gerrit-master/test_container_integration_gerrit_master.py
@@ -18,6 +18,8 @@ import re
 import requests
 import time
 
+import utils
+
 CONFIG_FILES = ["gerrit.config", "secure.config", "replication.config"]
 
 @pytest.fixture(scope="module")
@@ -75,13 +77,12 @@ def config_file_to_test(request):
 @pytest.mark.incremental
 class TestGerritMasterStartScript(object):
   def test_gerrit_master_gerrit_starts_up(self, container_run):
-    timeout = time.time() + 60
-    while time.time() < timeout:
-      last_log_line = container_run.logs().decode("utf-8")
-      if re.search(r"Gerrit Code Review .+ ready", last_log_line):
-        break
-      time.sleep(2)
-    assert timeout > time.time()
+    def wait_for_gerrit_start():
+      log = container_run.logs().decode("utf-8")
+      return log, re.search(r"Gerrit Code Review .+ ready", log)
+
+    finished_in_time, _ = utils.exec_fn_with_timeout(wait_for_gerrit_start, 60)
+    assert finished_in_time
 
   def test_gerrit_master_custom_gerrit_config_available(self,
       container_run, config_file_to_test):
