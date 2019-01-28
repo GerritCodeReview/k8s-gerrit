61341ff65c94b050582d92a81c032c1918a7311a
Thomas Draebing <thomas.draebing@sap.com>
2019-01-28 16:36:29 +0100

    Add a helper function for tests to wait for event
    A pattern used by many tests was to wait for a certain event happening
    in the container and to stay in a time limit. This was implemented in
    each test case using this pattern.
    
    This change adds a helper function available to all tests, that takes
    a function and a time limit. The function can return some output, which
    is forwarded to the test and has to return a boolean, indicating whether
    the event, for which the function tests has happened. The helper
    function will execute the given function as long as this function returns
    False or until the time limit is reached and will then return the
    functions output and a boolean indicating, whether the function returned
    True within the time limit.
    
    Change-Id: I3505e0f25028a81f84dc7e5b8f006e0d6a904ab4

---
+++ tests/helpers/utils.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+
+def exec_fn_with_timeout(func, limit=60):
+  """Helper function that executes a given function until it returns True or a
+     given time limit is reached.
+
+  Arguments:
+    fn {function} -- Funtion to execute. The function can return some output
+                    (or None) and as a second return value a boolean indicating,
+                    whether the event the function was waiting for has happened.
+
+  Keyword Arguments:
+    limit {int} -- Maximum time in seconds to wait for a positive response of
+                   the function (default: {60})
+
+  Returns:
+    boolean -- False, if the timeout was reached
+    any -- Last output of fn
+  """
+
+  timeout = time.time() + limit
+  while time.time() < timeout:
+    output, is_finished = func()
+    if is_finished:
+      return True, output
+  return False, output
