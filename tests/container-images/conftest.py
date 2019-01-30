2ae16e66a8182c587010a3f49ad548ee317bd217
Thomas Draebing <thomas.draebing@sap.com>
2019-01-30 10:40:56 +0100

    Provide a factory providing gerrit container
    So far the provisioning of a gerrit container with a factory was
    only performed in the gerrit-slave container tests. Such a
    container will also be needed to test the functionality of the
    gerrit-init container.
    
    This change moves the factory for a gerrit container out of the
    gerrit-slave container tests to the conftests.py file for
    container-image tests. Thereby, the factory is available for all
    container-image tests.
    
    Change-Id: Ica537347bef3ba72f0de408a81c5c226aaaee14d

--- tests/container-images/conftest.py
+++ tests/container-images/conftest.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os.path
 import time
 
 from sqlalchemy import create_engine
@@ -74,3 +75,66 @@ def mysql_container_factory():
     return MySQLContainer(docker_client, docker_network, host, port, root_pwd)
 
   return get_mysql_container
+
+class GerritContainer():
+
+  def __init__(self, docker_client, docker_network, tmp_dir, image,
+               configs, port):
+    self.docker_client = docker_client
+    self.docker_network = docker_network
+    self.tmp_dir = tmp_dir
+    self.image = image
+    self.configs = configs
+    self.port = port
+
+    self.gerrit_container = None
+
+    self._start_gerrit_container()
+
+  def _create_config_files(self):
+    tmp_config_dir = os.path.join(self.tmp_dir, "configs")
+    if not os.path.isdir(tmp_config_dir):
+      os.mkdir(tmp_config_dir)
+    config_paths = {}
+    for filename, content in self.configs.items():
+      gerrit_config_file = os.path.join(tmp_config_dir, filename)
+      with open(gerrit_config_file, "w") as config_file:
+        config_file.write(content)
+      config_paths[filename] = gerrit_config_file
+    return config_paths
+
+  def _define_volume_mounts(self):
+    volumes = {v: {
+      "bind": "/var/config/%s" % k,
+      "mode": "rw"
+    } for (k, v) in self._create_config_files().items()}
+    volumes[os.path.join(self.tmp_dir, "lib")] = {
+      "bind": "/var/gerrit/lib",
+      "mode": "rw"
+    }
+    return volumes
+
+  def _start_gerrit_container(self):
+    self.gerrit_container = self.docker_client.containers.run(
+      image=self.image.id,
+      user="gerrit",
+      volumes=self._define_volume_mounts(),
+      ports={
+        str(self.port): str(self.port)
+      },
+      network=self.docker_network.name,
+      detach=True,
+      auto_remove=True
+    )
+
+  def stop_gerrit_container(self):
+    self.gerrit_container.stop(timeout=1)
+
+@pytest.fixture(scope="session")
+def gerrit_container_factory():
+  def get_gerrit_container(docker_client, docker_network, tmp_dir, image,
+                           gerrit_config, port):
+    return GerritContainer(docker_client, docker_network, tmp_dir, image,
+                           gerrit_config, port)
+
+  return get_gerrit_container
