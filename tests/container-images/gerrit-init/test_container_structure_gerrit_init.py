a119ed48f06d665408380d1114617f1177c7f3b7
Thomas Draebing <thomas.draebing@sap.com>
2019-02-21 17:23:58 +0100

    Do not reindex Lucene indices on every start of the gerrit-master pod
    The gerrit-master pod was reindexing all Gerrit indices on each startup.
    This could be expensive, if the amount of data in the indices would be
    large.
    
    This change moves the reindexing to the gerrit-init container.
    A python-script was added, that runs reindexing, if indices are not
    ready. This is the case in the following cases:
    
    - The $gerrit_site/index/gerrit_index.config does not exist
    - At least one of the indices is marked as not ready in the
      gerrit_index.config file.
    - The index versions are different between the Lucene indices and the
      gerrit_index.config
    
    Further if the chart is configured to not persist the Gerrit site,
    reindexing will be enforced.
    
    Change-Id: I65e74f6b2d55788737fa5b67b61b1d25e6ebb925

--- tests/container-images/gerrit-init/test_container_structure_gerrit_init.py
+++ tests/container-images/gerrit-init/test_container_structure_gerrit_init.py
@@ -24,7 +24,11 @@ def container_run(docker_client, container_endless_run_factory, gerrit_init_imag
 
 @pytest.fixture(
     scope="function",
-    params=["/var/tools/gerrit_init.py", "/var/tools/git_config_parser.py"],
+    params=[
+        "/var/tools/gerrit_init.py",
+        "/var/tools/gerrit_reindex.py",
+        "/var/tools/git_config_parser.py",
+    ],
 )
 def expected_script(request):
     return request.param
