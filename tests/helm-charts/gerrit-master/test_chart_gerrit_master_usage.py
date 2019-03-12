45515fbc0affc265336e4e94074dfc6a65391238
Thomas Draebing <thomas.draebing@sap.com>
2019-03-12 10:05:52 +0100

    Add test for gerrit-master chart creating and cloning a repo
    This change adds integration tests for the gerrit-master chart, which
    test whether creation of a project via the Rest API and the subsequent
    cloning of this project works. These tests should validate that basic
    Gerrit functionality is working using the helm chart to set up a Gerrit
    deployment.
    
    Change-Id: I288578ed77f823043703fe66c610a98bb60a20dc

---
+++ tests/helm-charts/gerrit-master/test_chart_gerrit_master_usage.py
@@ -0,0 +1,54 @@
+# pylint: disable=W0613
+
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os.path
+
+import git
+import pytest
+import requests
+
+
+@pytest.mark.slow
+@pytest.mark.incremental
+@pytest.mark.integration
+@pytest.mark.kubernetes
+class TestGerritMasterChartSetup:
+    @pytest.mark.timeout(240)
+    def test_create_project_rest(
+        self, request, test_cluster, gerrit_master_ready_deployment
+    ):
+        create_project_url = "http://master.%s/a/projects/test" % (
+            request.config.getoption("--ingress-url")
+        )
+        response = None
+
+        while not response:
+            try:
+                response = requests.put(create_project_url, auth=("admin", "secret"))
+            except requests.exceptions.ConnectionError:
+                continue
+
+        assert response.status_code == 201
+
+    def test_cloning_project(
+        self, request, tmp_path_factory, test_cluster, gerrit_master_ready_deployment
+    ):
+        clone_dest = tmp_path_factory.mktemp("gerrit_master_chart_clone_test")
+        repo_url = "http://master.%s/test.git" % (
+            request.config.getoption("--ingress-url")
+        )
+        repo = git.Repo.clone_from(repo_url, clone_dest)
+        assert repo.git_dir == os.path.join(clone_dest, ".git")
