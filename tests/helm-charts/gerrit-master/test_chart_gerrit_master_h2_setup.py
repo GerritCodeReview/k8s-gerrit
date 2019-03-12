99970f384a755cfb0d75bb84c952caca01f39ad7
Thomas Draebing <thomas.draebing@sap.com>
2019-03-12 10:05:52 +0100

    Add test for gerrit-master chart creating and cloning a repo
    This change adds integration tests for the gerrit-master chart, which
    test whether creation of a project via the Rest API and the subsequent
    cloning of this project works. These tests should validate that basic
    Gerrit functionality is working using the helm chart to set up a Gerrit
    deployment.
    
    Change-Id: I288578ed77f823043703fe66c610a98bb60a20dc

--- tests/helm-charts/gerrit-master/test_chart_gerrit_master_h2_setup.py
+++ tests/helm-charts/gerrit-master/test_chart_gerrit_master_h2_setup.py
@@ -14,11 +14,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-def test_deployment(test_cluster, gerrit_master_h2_deployment):
-  installed_charts = test_cluster.helm.list()
-  gerrit_master_chart = None
-  for chart in installed_charts:
-    if chart["Name"] == "gerrit-master":
-      gerrit_master_chart = chart
-  assert gerrit_master_chart is not None
-  assert gerrit_master_chart["Status"] == "DEPLOYED"
+import time
+
+import pytest
+import requests
+
+from kubernetes import client, config
+
+import utils
+
+@pytest.mark.slow
+@pytest.mark.incremental
+class TestGerritMasterChartH2(object):
+  def test_deployment(self, test_cluster, gerrit_master_h2_deployment):
+    installed_charts = test_cluster.helm.list()
+    gerrit_master_chart = None
+    for chart in installed_charts:
+      if chart["Name"] == "gerrit-master":
+        gerrit_master_chart = chart
+    assert gerrit_master_chart is not None
+    assert gerrit_master_chart["Status"] == "DEPLOYED"
+
+  def test_gerrit_pod_gets_ready(self, request, test_cluster,
+                                 gerrit_master_h2_deployment):
+    def wait_for_readiness():
+      core_v1 = client.CoreV1Api()
+      pod_list = core_v1.list_pod_for_all_namespaces(watch=False, labe)
+      core_v1.read_namespaced_pod_status(name, "default")
+
+    finished_in_time, _ = utils.exec_fn_with_timeout(wait_for_readiness)
+    assert finished_in_time
+
+  def test_create_project_rest(self, request, test_cluster,
+                               gerrit_master_h2_deployment):
+    time.sleep(60)
+    create_project_url = "http://master.%s/a/projects/test" % (
+      request.config.getoption("--ingress-url"))
+    response = requests.put(create_project_url, auth=('admin', 'secret'))
+    print(response.text)
+    assert response.status_code == 201
