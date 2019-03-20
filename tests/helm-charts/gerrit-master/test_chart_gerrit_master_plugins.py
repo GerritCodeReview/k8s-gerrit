5d9d83f4485e973d19ad79c26ec9eccbf30a5151
Thomas Draebing <thomas.draebing@sap.com>
2019-03-20 13:20:03 +0100

    Allow to install custom Gerrit-plugins in gerrit-master chart
    It was not yet possible to install plugins that were not core plugins in
    the gerrit-master chart.
    
    This change allows to configure a list of plugins to install for Gerrit.
    To do this, it is required to give a URL, where the plugin jar can be
    downloaded. Optionally, a SHA256-checksum can be provided to ensure that
    the same jar is used for each pod. The plugins are downloaded in an
    init-container before Gerrit starts up, as long as the jar is not yet
    present in the plugins directory.
    
    Change-Id: I34e6bc5668090b37c9749d4df72eeb719fdf3807

--- tests/helm-charts/gerrit-master/test_chart_gerrit_master_plugins.py
+++ tests/helm-charts/gerrit-master/test_chart_gerrit_master_plugins.py
@@ -14,13 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 import json
 import time
+import urllib.request
 
 import pytest
 import requests
 
+from kubernetes import client
+
 import utils
+
+PLUGINS = ["autosubmitter", "reviewers"]
+GERRIT_VERSION = "2.16"
+
+@pytest.fixture(scope="module")
+def plugin_list():
+  plugin_list = list()
+  for plugin in PLUGINS:
+    url = "https://gerrit-ci.gerritforge.com/view/Plugins-stable-{gerrit_version}/job/plugin-{plugin}-bazel-stable-{gerrit_version}/lastSuccessfulBuild/artifact/bazel-genfiles/plugins/{plugin}/{plugin}.jar".format(
+      plugin=plugin, gerrit_version=GERRIT_VERSION)
+    jar = urllib.request.urlopen(url).read()
+    plugin_list.append({
+      "name": plugin,
+      "url": url,
+      "sha256": hashlib.sha256(jar).hexdigest()
+    })
+  return plugin_list
 
 @pytest.fixture(scope="class")
 def gerrit_master_deployment_with_plugins_factory(
@@ -64,28 +85,54 @@ def gerrit_master_deployment_with_core_plugins(
 
   test_cluster.helm.delete(chart["name"])
 
-@pytest.fixture(scope="class")
-def gerrit_master_deployment_update_remove_core_plugin(
-    test_cluster, gerrit_master_deployment_with_custom_plugins):
-  chart = gerrit_master_deployment_with_custom_plugins
-  chart["removed_plugins"] = chart["installed_plugins"].pop()
+@pytest.fixture(
+  scope="class",
+  params=[1, 2],
+  ids=[
+    "single-other-plugin",
+    "multiple-other-plugin"
+  ])
+def gerrit_master_deployment_with_other_plugins(
+    request, docker_tag, test_cluster, plugin_list,
+    gerrit_master_deployment_with_plugins_factory):
   chart_opts = {
-    "gerritMaster.plugins.core": "{%s}" % (",".join(chart["installed_plugins"]))
+    "images.registry.name": request.config.getoption("--registry"),
+    "images.version": docker_tag,
+    "images.ImagePullPolicy": "IfNotPresent",
+    "gerritMaster.ingress.host": "master.%s" % request.config.getoption("--ingress-url")
   }
-  test_cluster.helm.upgrade(
-    chart=chart["chart"],
-    name=chart["name"],
-    set_values=chart_opts,
-    reuse_values=True,
-    fail_on_err=True
-  )
-  pod_labels = "app=gerrit-master,release=%s" % chart["name"]
-  finished_in_time = utils.wait_for_pod_readiness(pod_labels, timeout=300)
-  if not finished_in_time:
-    raise utils.TimeOutException("Gerrit master pod was not ready in time.")
-  time.sleep(5)
+  selected_plugins = plugin_list[:request.param]
+  for counter, plugin in enumerate(selected_plugins):
+    chart_opts["gerritMaster.plugins.other[%d].name" % counter] = plugin["name"]
+    chart_opts["gerritMaster.plugins.other[%d].url" % counter] = plugin["url"]
+    chart_opts["gerritMaster.plugins.other[%d].sha256" % counter] = plugin["sha256"]
+  chart = gerrit_master_deployment_with_plugins_factory(chart_opts)
+  chart["installed_plugins"] = selected_plugins
 
   yield chart
+
+  test_cluster.helm.delete(chart["name"])
+
+@pytest.fixture()
+def gerrit_master_deployment_with_other_plugin_wrong_sha(
+    request, docker_tag, test_cluster, plugin_list,
+    gerrit_master_h2_deployment_factory):
+  chart_opts = {
+    "images.registry.name": request.config.getoption("--registry"),
+    "images.version": docker_tag,
+    "images.ImagePullPolicy": "IfNotPresent",
+    "gerritMaster.ingress.host": "master.%s" % request.config.getoption("--ingress-url")
+  }
+  plugin = plugin_list[0]
+  chart_opts["gerritMaster.plugins.other[0].name"] = plugin["name"]
+  chart_opts["gerritMaster.plugins.other[0].url"] = plugin["url"]
+  chart_opts["gerritMaster.plugins.other[0].sha256"] = "notAValidSha"
+  chart = gerrit_master_h2_deployment_factory(chart_opts)
+  chart["installed_plugins"] = plugin
+
+  yield chart
+
+  test_cluster.helm.delete(chart["name"])
 
 def update_chart(helm, chart, opts):
   helm.upgrade(
@@ -136,3 +183,69 @@ class TestGerritMasterChartCorePluginInstall(object):
       request.config.getoption("--ingress-url")))
     assert chart["removed_plugin"] not in response
     self._assert_installed_plugins(chart["installed_plugins"], response)
+
+@pytest.mark.slow
+@pytest.mark.incremental
+class TestGerritMasterChartOtherPluginInstall(object):
+  def _remove_plugin_from_install_list(self, installed_plugins):
+    removed_plugin = installed_plugins.pop()
+    plugin_install_list = dict()
+    if installed_plugins:
+      for counter, plugin in enumerate(installed_plugins):
+        plugin_install_list["gerritMaster.plugins.other[%d].name" % counter] = \
+          plugin["name"]
+        plugin_install_list["gerritMaster.plugins.other[%d].url" % counter] = \
+          plugin["url"]
+        plugin_install_list["gerritMaster.plugins.other[%d].sha256" % counter] = \
+          plugin["sha256"]
+    else:
+      plugin_install_list["gerritMaster.plugins.other"] = "{}"
+    return plugin_install_list, removed_plugin, installed_plugins
+
+  def _assert_installed_plugins(self, expected_plugins, installed_plugins):
+    for plugin in expected_plugins:
+      assert plugin["name"] in installed_plugins
+      assert installed_plugins[plugin["name"]]["filename"] == "%s.jar" % plugin["name"]
+
+  def test_install_other_plugins(self, request, test_cluster,
+                                 gerrit_master_deployment_with_other_plugins):
+    response = get_gerrit_plugin_list("http://master.%s" % (
+      request.config.getoption("--ingress-url")))
+    self._assert_installed_plugins(
+      gerrit_master_deployment_with_other_plugins["installed_plugins"], response)
+
+  def test_install_core_plugins_are_removed_with_update(
+      self, request, test_cluster, gerrit_master_deployment_with_other_plugins):
+    chart = gerrit_master_deployment_with_other_plugins
+    chart_opts, chart["removed_plugin"], chart["installed_plugin"] = \
+      self._remove_plugin_from_install_list(chart["installed_plugins"])
+    update_chart(
+      test_cluster.helm, chart, chart_opts
+    )
+    response = get_gerrit_plugin_list("http://master.%s" % (
+      request.config.getoption("--ingress-url")))
+    assert chart["removed_plugin"]["name"] not in response
+    self._assert_installed_plugins(chart["installed_plugins"], response)
+
+def test_install_other_plugins_fails_wrong_sha(
+    request, test_cluster, gerrit_master_deployment_with_other_plugin_wrong_sha):
+  pod_labels = "app=gerrit-master,release=%s" % (
+    gerrit_master_deployment_with_other_plugin_wrong_sha["name"])
+  core_v1 = client.CoreV1Api()
+  pod_list = core_v1.list_pod_for_all_namespaces(
+    watch=False, label_selector=pod_labels)
+  if len(pod_list.items) > 1:
+    raise RuntimeError("Too many gerrit-master pods with the same release name.")
+  pod_name = pod_list.items[0].metadata.name
+  current_status = None
+  timeout = time.time() + 180
+  while time.time() < timeout:
+    pod = core_v1.read_namespaced_pod_status(
+      pod_name, gerrit_master_deployment_with_other_plugin_wrong_sha["namespace"])
+    for init_container_status in pod.status.init_container_statuses:
+      if init_container_status.name == "install-plugins" and init_container_status.state.terminated:
+        current_status = init_container_status
+        assert current_status.state.terminated.exit_code > 0
+        return
+
+  assert current_status.state.terminated.exit_code > 0
