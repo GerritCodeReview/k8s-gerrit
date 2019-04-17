dd2fcceabd1cbea7c1087e004ce907c251dcc528
Thomas Draebing <thomas.draebing@sap.com>
2019-04-17 09:47:50 +0200

    Share gerrit_index.config between pods when using elasticsearch
    When a new pod was added to the StatefulSet, the indices were
    reindexed, which could become expensive, if the indices were
    large.
    
    To avoid this, the gerrit_index.config is now shared between Gerrit
    pods and reindexing will only happen, if the file does not exist
    or indices are not ready.
    
    Change-Id: Idb2ec8965e2b4c4bdd84902e9934652553db70b0

--- container-images/gerrit-init/tools/gerrit_init.py
+++ container-images/gerrit-init/tools/gerrit_init.py
@@ -23,7 +23,7 @@ from download_plugins import get_installer
 from git_config_parser import GitConfigParser
 from init_config import InitConfig
 from log import get_logger
-from gerrit_reindex import IndexType, GerritElasticSearchReindexer
+from gerrit_reindex import IndexType
 
 LOG = get_logger("init")
 
@@ -37,6 +37,7 @@ class GerritInit:
 
         self.gerrit_config = self._parse_gerrit_config()
         self.installed_plugins = self._get_installed_plugins()
+        self.index_type = self._determine_index_type()
 
     def _parse_gerrit_config(self):
         gerrit_config_path = os.path.join(self.site, "etc/gerrit.config")
@@ -45,6 +46,13 @@ class GerritInit:
             return GitConfigParser(gerrit_config_path)
 
         return None
+
+    def _determine_index_type(self):
+        if self.gerrit_config:
+            index_type = self.gerrit_config.get("index.type", "lucene").upper()
+            return IndexType[index_type]
+
+        return IndexType["LUCENE"]
 
     def _get_gerrit_version(self, gerrit_war_path):
         command = "java -jar %s version" % gerrit_war_path
@@ -132,13 +140,6 @@ class GerritInit:
                 init_process.returncode,
             )
             sys.exit(1)
-
-        if not self.gerrit_config:
-            self.gerrit_config = self._parse_gerrit_config()
-        index_type = self.gerrit_config.get("index.type", IndexType.LUCENE.name)
-        if IndexType[index_type.upper()] is IndexType.ELASTICSEARCH:
-            reindexer = GerritElasticSearchReindexer(self.site)
-            reindexer.start(is_forced=True)
 
 
 # pylint: disable=C0103
