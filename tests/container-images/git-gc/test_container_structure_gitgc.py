c5dca4e0c2820a708ef29c8f058a8aa523181c47
Thomas Draebing <thomas.draebing@sap.com>
2019-07-05 11:15:21 +0200

    Resolve pylint message R0801
    R0801: Similar lines in %s files
    
    Reduced some code duplication and configured the warning only to occur
    with 6 similar lines.
    
    Change-Id: I132b1b8eb44c7ff01ee3bc6a1251882b4edcf279

--- tests/container-images/git-gc/test_container_structure_gitgc.py
+++ tests/container-images/git-gc/test_container_structure_gitgc.py
@@ -15,24 +15,10 @@
 import pytest
 
 @pytest.fixture(scope="module")
-def container_run(request, docker_client, gitgc_image):
-  print("Starting git-gc-container...")
-  container_run = docker_client.containers.run(
-    image=gitgc_image.id,
-    entrypoint="/bin/bash",
-    command=["-c", "tail -f /dev/null"],
-    user="gerrit",
-    detach=True,
-    auto_remove=True
-  )
-
-  def stop_container():
-    print("Stopping git-gc-container...")
-    container_run.stop(timeout=1)
-
-  request.addfinalizer(stop_container)
-
-  return container_run
+def container_run(docker_client, container_endless_run_factory, gitgc_image):
+  container_run = container_endless_run_factory(docker_client, gitgc_image)
+  yield container_run
+  container_run.stop(timeout=1)
 
 
 def test_gitgc_inherits_from_base(gitgc_image):
