aabf63a651868aee138bbf0b4fd5e607407f78b0
Thomas Draebing <thomas.draebing@sap.com>
2020-12-09 16:08:53 +0100

    Install pull-replication plugin
    Change-Id: Iae6b0988867d93902a0a35c8ed5e44a45fcc620f

--- tests/conftest.py
+++ tests/conftest.py
@@ -365,6 +365,7 @@ def required_plugins(request):
         "container": [
             "healthcheck",
             "kafka-events",
+            "pull-replication",
             "websession-broker",
             "zookeeper-refdb",
         ],
