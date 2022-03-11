d0a8fe98fb5f900fe62a61479f95c13e537d965b
Thomas Draebing <thomas.draebing@sap.com>
2022-03-11 15:40:36 +0100

    Add k8s operator for Git GC
    The plan is to use the operator pattern to manage Gerrit installations
    in the future instead of relying only on helm.
    
    This change adds a minimal operator using the Java Operator SDK.
    It allows to create and delete GitGC CronJobs. This was chosen as
    a starting point, since the scope is much smaller than Gerrit itself.
    
    Change-Id: Id8f55be50c67ed27e087c9f6c00ba6aa18618db5

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/GerritOperator.java
@@ -0,0 +1,44 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator;
+
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.Operator;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.takes.facets.fork.FkRegex;
+import org.takes.facets.fork.TkFork;
+import org.takes.http.Exit;
+import org.takes.http.FtBasic;
+
+public class GerritOperator {
+  private static final Logger log = LoggerFactory.getLogger(GerritOperator.class);
+
+  public static void main(String[] args) throws IOException {
+    Config config = new ConfigBuilder().withNamespace(null).build();
+    KubernetesClient client = new DefaultKubernetesClient(config);
+    Operator operator = new Operator(client);
+    log.debug("Registering GitGc Reconciler");
+    operator.register(new GitGcReconciler(client));
+    operator.installShutdownHook();
+    operator.start();
+
+    new FtBasic(new TkFork(new FkRegex("/health", "ALL GOOD.")), 8080).start(Exit.NEVER);
+  }
+}
