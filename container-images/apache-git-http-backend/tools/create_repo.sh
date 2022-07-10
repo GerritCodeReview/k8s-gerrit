7aaae5e366aa091bef209ee252d841829c79c125
Masaki Muranaka <monaka@monami-ya.com>
2022-07-10 15:36:46 +0900

    Stop calling `git init` by `/new` if the repository available.
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>
    Change-Id: Iee3047b401184b8cbfc5889848f73692938de37d

--- container-images/apache-git-http-backend/tools/create_repo.sh
+++ container-images/apache-git-http-backend/tools/create_repo.sh
@@ -8,13 +8,19 @@ if test "$REPO" == "${REPO%.git}"; then
 fi
 
 STATUS_CODE="500 Internal Server Error"
-MESSAGE="Repository could not be created."
+MESSAGE="May bugs in the script."
 
-git init --bare /var/gerrit/git/${REPO} > /dev/null
-
-if test -f /var/gerrit/git/${REPO}/HEAD; then
-    STATUS_CODE="201 Created"
-    MESSAGE="Repository successfully created."
+if test -d /var/gerrit/git/${REPO}; then
+    STATUS_CODE="202 Accepted"
+    MESSAGE="Repository already created."
+else
+    git init --bare /var/gerrit/git/${REPO} > /dev/null
+    if test -f /var/gerrit/git/${REPO}/HEAD; then
+        STATUS_CODE="201 Created"
+        MESSAGE="Repository successfully created."
+    else
+        MESSAGE="Repository could not be created."
+    fi
 fi
 
 echo "Status: ${STATUS_CODE}"
