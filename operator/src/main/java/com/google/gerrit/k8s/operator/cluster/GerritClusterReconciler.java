947ce98374532268615ff0a97480956b6bebdd79
Thomas Draebing <thomas.draebing@sap.com>
2022-08-04 09:31:12 +0200

    Use a dependent resource for the git repository PVC
    The java operator SDK provides a workflow feature [1]. This feature
    allows more control over how components depend on each other and
    reduces the need for boilerplate code.
    
    [1] https://javaoperatorsdk.io/docs/workflows
    
    Change-Id: Iaa5d6e9b5fe58de5cd6d0ca118e28ca655668a92

--- operator/src/main/java/com/google/gerrit/k8s/operator/cluster/GerritClusterReconciler.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/cluster/GerritClusterReconciler.java
@@ -14,26 +14,20 @@
 
 package com.google.gerrit.k8s.operator.cluster;
 
-import com.google.common.flogger.FluentLogger;
-import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
-import io.fabric8.kubernetes.api.model.PersistentVolumeClaimBuilder;
+import static com.google.gerrit.k8s.operator.cluster.GitRepositoriesPVC.REPOSITORY_PVC_NAME;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.javaoperatorsdk.operator.api.reconciler.Cleaner;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 
-@ControllerConfiguration
-public class GerritClusterReconciler implements Reconciler<GerritCluster>, Cleaner<GerritCluster> {
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-
-  public static final String REPOSITORY_PVC_NAME = "git-repositories-pvc";
-
+@ControllerConfiguration(
+    dependents = {
+      @Dependent(type = GitRepositoriesPVC.class),
+    })
+public class GerritClusterReconciler implements Reconciler<GerritCluster> {
   private final KubernetesClient kubernetesClient;
 
   public GerritClusterReconciler(KubernetesClient client) {
@@ -43,77 +37,28 @@ public class GerritClusterReconciler implements Reconciler<GerritCluster>, Clean
   @Override
   public UpdateControl<GerritCluster> reconcile(
       GerritCluster gerritCluster, Context<GerritCluster> context) {
-    String ns = gerritCluster.getMetadata().getNamespace();
-    String name = gerritCluster.getMetadata().getName();
-    logger.atInfo().log("Reconciling Gerrit cluster with name: %s/%s", ns, name);
-
-    createOrReplaceGitRepositoryStorage(gerritCluster);
-
-    return UpdateControl.updateStatus(updateStatus(gerritCluster));
-  }
-
-  private void createOrReplaceGitRepositoryStorage(GerritCluster gerritCluster) {
-    checkStorageClass(
-        gerritCluster.getSpec().getStorageClasses().getReadWriteOnce(),
-        gerritCluster.getSpec().getStorageClasses().getReadWriteMany());
-
-    PersistentVolumeClaim gitRepoPvc =
-        new PersistentVolumeClaimBuilder()
-            .withNewMetadata()
-            .withName(REPOSITORY_PVC_NAME)
-            .withLabels(
-                gerritCluster.getLabels(
-                    "git-repositories-storage", this.getClass().getSimpleName()))
-            .endMetadata()
-            .withNewSpec()
-            .withAccessModes("ReadWriteMany")
-            .withNewResources()
-            .withRequests(
-                Map.of("storage", gerritCluster.getSpec().getGitRepositoryStorage().getSize()))
-            .endResources()
-            .withStorageClassName(gerritCluster.getSpec().getStorageClasses().getReadWriteMany())
-            .withSelector(gerritCluster.getSpec().getGitRepositoryStorage().getSelector())
-            .withVolumeName(gerritCluster.getSpec().getGitRepositoryStorage().getVolumeName())
-            .endSpec()
-            .build();
-
-    kubernetesClient
-        .resource(gitRepoPvc)
-        .inNamespace(gerritCluster.getMetadata().getNamespace())
-        .createOrReplace();
+    return UpdateControl.patchStatus(updateStatus(gerritCluster));
   }
 
   private GerritCluster updateStatus(GerritCluster gerritCluster) {
-    GerritClusterStatus status = gerritCluster.getStatus();
-    if (kubernetesClient.persistentVolumeClaims().withName(REPOSITORY_PVC_NAME).get() != null) {
+    GerritClusterStatus status;
+    if (gerritCluster.getStatus() == null) {
+      status = new GerritClusterStatus();
+    } else {
+      status = gerritCluster.getStatus();
+    }
+
+    if (kubernetesClient
+            .persistentVolumeClaims()
+            .inNamespace(gerritCluster.getMetadata().getNamespace())
+            .withName(REPOSITORY_PVC_NAME)
+            .get()
+        != null) {
       status.setRepositoryPvcName(REPOSITORY_PVC_NAME);
     } else {
       status.setRepositoryPvcName(null);
     }
     gerritCluster.setStatus(status);
     return gerritCluster;
-  }
-
-  private void checkStorageClass(String... storageClassNames) {
-    List<String> storageClasses =
-        kubernetesClient.storage().storageClasses().list().getItems().stream()
-            .map(sc -> sc.getMetadata().getName())
-            .collect(Collectors.toList());
-
-    for (String sc : storageClassNames) {
-      if (!storageClasses.contains(sc)) {
-        logger.atWarning().log("Storageclass %s not found.", sc);
-      }
-    }
-  }
-
-  @Override
-  public DeleteControl cleanup(GerritCluster gerritCluster, Context<GerritCluster> context) {
-    kubernetesClient
-        .persistentVolumeClaims()
-        .inNamespace(gerritCluster.getMetadata().getNamespace())
-        .withName(gerritCluster.getMetadata().getName())
-        .delete();
-    return DeleteControl.defaultDelete();
   }
 }
