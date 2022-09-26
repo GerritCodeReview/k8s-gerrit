5e7bb1ba51e270e9e4e061681c8d402f696a58aa
Thomas Draebing <thomas.draebing@sap.com>
2022-09-26 16:43:50 +0200

    Update java-operator-sdk to 4.1.0
    This includes support for managing two dependent resources of the same
    type in a proper way.
    The full changelog can be found here:
    https://github.com/java-operator-sdk/java-operator-sdk/compare/v3.1.1...v4.1.0
    
    Since java-operator-sdk 4 uses fabric8 kubernetes-client 6.2.0, the
    kubernetes client was updated as well. Changelog:
    https://github.com/fabric8io/kubernetes-client/blob/v6.2.0/CHANGELOG.md
    
    Change-Id: Ic9609537755b6694111121bd6e0dd19d63ec44c8

--- operator/src/main/java/com/google/gerrit/k8s/operator/cluster/GerritLogsPVC.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/cluster/GerritLogsPVC.java
@@ -21,7 +21,7 @@ import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernete
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
 import java.util.Map;
 
-@KubernetesDependent(labelSelector = "app.kubernetes.io/component=gerrit-logs-storage")
+@KubernetesDependent(resourceDiscriminator = GerritLogsPVCDiscriminator.class)
 public class GerritLogsPVC
     extends CRUDKubernetesDependentResource<PersistentVolumeClaim, GerritCluster> {
 
