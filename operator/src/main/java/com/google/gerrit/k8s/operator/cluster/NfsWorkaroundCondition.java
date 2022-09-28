9a2876d72d3ca1ee0bdd71296c5a97d4d77922f7
Thomas Draebing <thomas.draebing@sap.com>
2022-09-28 09:21:48 +0200

    [Operator] Make NfsIdmapdConfigMap a managed dependent resource
    Change-Id: If21674e68c37b159fe65c5f4b72bf0036d7190dc

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/cluster/NfsWorkaroundCondition.java
@@ -0,0 +1,29 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.cluster;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+
+public class NfsWorkaroundCondition implements Condition<ConfigMap, GerritCluster> {
+
+  @Override
+  public boolean isMet(
+      GerritCluster gerritCluster, ConfigMap secondary, Context<GerritCluster> context) {
+    NfsWorkaroundConfig cfg = gerritCluster.getSpec().getStorageClasses().getNfsWorkaround();
+    return cfg.isEnabled() && cfg.getIdmapdConfig() != null;
+  }
+}
