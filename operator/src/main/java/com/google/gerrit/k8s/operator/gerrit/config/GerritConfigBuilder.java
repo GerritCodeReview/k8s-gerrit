412db3fc3e91d648c8c86e6be00fc9c7a4f52780
Thomas Draebing <thomas.draebing@sap.com>
2022-10-18 15:28:22 +0200

    [Operator] Enforce required configuration options
    Some options like container.javaHome in the gerrit.config are defined
    by the Kubernetes deployment and changing them would break the setup.
    
    The operator now checks whether the provided gerrit.config is valid.
    If a required configuration is set to an illegal value, the operator
    will throw an exception and not reconcile the Gerrit deployment. If
    the option is set to the expected value, the operator will do nothing.
    If the option is not set,the operator will set the value.
    
    Change-Id: I619ab47875555c5e5e10e8075880904f12b505b5

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/config/GerritConfigBuilder.java
@@ -0,0 +1,89 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.gerrit.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+
+@SuppressWarnings("rawtypes")
+public class GerritConfigBuilder {
+  private static final List<RequiredOption> requiredOptions =
+      new ArrayList<>(setupStaticRequiredOptions());
+
+  private static List<RequiredOption> setupStaticRequiredOptions() {
+    List<RequiredOption> requiredOptions = new ArrayList<>();
+    requiredOptions.add(
+        new RequiredOption<String>("container", "javaHome", "/usr/lib/jvm/java-11-openjdk"));
+    requiredOptions.add(
+        new RequiredOption<Set<String>>(
+            "container",
+            "javaOptions",
+            Set.of("-Djavax.net.ssl.trustStore=/var/gerrit/etc/keystore")));
+    requiredOptions.add(new RequiredOption<String>("container", "user", "gerrit"));
+    requiredOptions.add(new RequiredOption<String>("gerrit", "basepath", "git"));
+    requiredOptions.add(new RequiredOption<Boolean>("index", "onlineUpgrade", false));
+    return requiredOptions;
+  }
+
+  public static Config buildFromText(String text) {
+    Config cfg = new Config();
+    try {
+      cfg.fromText(text);
+    } catch (ConfigInvalidException e) {
+      throw new IllegalStateException("The provided gerrit.config is invalid.");
+    }
+
+    return buildFromConfig(cfg);
+  }
+
+  public static Config buildFromConfig(Config cfg) {
+    GerritConfigValidator configValidator = new GerritConfigValidator(requiredOptions);
+    configValidator.check(cfg);
+
+    return setRequiredOptions(cfg);
+  }
+
+  @SuppressWarnings("unchecked")
+  private static Config setRequiredOptions(Config cfg) {
+    for (RequiredOption<?> opt : requiredOptions) {
+      if (opt.getExpected() instanceof String) {
+        cfg.setString(
+            opt.getSection(), opt.getSubSection(), opt.getKey(), (String) opt.getExpected());
+      } else if (opt.getExpected() instanceof Boolean) {
+        cfg.setBoolean(
+            opt.getSection(), opt.getSubSection(), opt.getKey(), (Boolean) opt.getExpected());
+      } else if (opt.getExpected() instanceof Set) {
+        List<String> values =
+            new ArrayList<String>(
+                Arrays.asList(
+                    cfg.getStringList(opt.getSection(), opt.getSubSection(), opt.getKey())));
+        List<String> expectedSet = new ArrayList<String>();
+        expectedSet.addAll((Set<String>) opt.getExpected());
+        expectedSet.removeAll(values);
+        values.addAll(expectedSet);
+        cfg.setStringList(opt.getSection(), opt.getSubSection(), opt.getKey(), values);
+      }
+    }
+    return cfg;
+  }
+
+  public static List<RequiredOption> getRequiredOptions() {
+    return requiredOptions;
+  }
+}
