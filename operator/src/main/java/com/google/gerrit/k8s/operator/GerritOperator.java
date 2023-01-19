66b6fdf0ea81b07b528143b240d120cdc2dd2061
Thomas Draebing <thomas.draebing@sap.com>
2023-01-19 12:58:00 +0100

    [Operator] Add ValidationWebhook for GitGc
    Change-Id: I4329f5462757d02390157af3074625fa47b2b7ef

--- operator/src/main/java/com/google/gerrit/k8s/operator/GerritOperator.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/GerritOperator.java
@@ -14,12 +14,20 @@
 
 package com.google.gerrit.k8s.operator;
 
+import static com.google.gerrit.k8s.operator.server.HttpServer.PORT;
+
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.ServicePort;
+import io.fabric8.kubernetes.api.model.ServicePortBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import java.util.Map;
 import java.util.Set;
 
 @Singleton
@@ -29,27 +37,70 @@ public class GerritOperator {
   public static final int SERVICE_PORT = 8080;
 
   private final KubernetesClient client;
+  private final LifecycleManager lifecycleManager;
 
   @SuppressWarnings("rawtypes")
   private final Set<Reconciler> reconcilers;
 
+  private final String namespace;
+
   private Operator operator;
+  private Service svc;
 
   @SuppressWarnings("rawtypes")
   @Inject
-  public GerritOperator(KubernetesClient client, Set<Reconciler> reconcilers) {
+  public GerritOperator(
+      LifecycleManager lifecycleManager,
+      KubernetesClient client,
+      Set<Reconciler> reconcilers,
+      @Named("Namespace") String namespace) {
+    this.lifecycleManager = lifecycleManager;
     this.client = client;
     this.reconcilers = reconcilers;
+    this.namespace = namespace;
   }
 
   public void start() throws Exception {
+    logger.atInfo().log(System.getenv().toString());
     operator = new Operator(client);
     for (Reconciler<?> reconciler : reconcilers) {
       logger.atInfo().log(
           String.format("Registering reconciler: %s", reconciler.getClass().getSimpleName()));
       operator.register(reconciler);
     }
-    operator.installShutdownHook();
     operator.start();
+    lifecycleManager.addShutdownHook(2, new Thread(this::shutdown));
+    lifecycleManager.addShutdownHook(999, new Thread(operator::stop));
+    applyService();
+  }
+
+  public void shutdown() {
+    client.resource(svc).delete();
+  }
+
+  private void applyService() {
+    ServicePort port =
+        new ServicePortBuilder()
+            .withName("http")
+            .withPort(SERVICE_PORT)
+            .withNewTargetPort(PORT)
+            .withProtocol("TCP")
+            .build();
+    svc =
+        new ServiceBuilder()
+            .withApiVersion("v1")
+            .withNewMetadata()
+            .withName(SERVICE_NAME)
+            .withNamespace(namespace)
+            .endMetadata()
+            .withNewSpec()
+            .withType("ClusterIP")
+            .withPorts(port)
+            .withSelector(Map.of("app", "gerrit-operator"))
+            .endSpec()
+            .build();
+
+    logger.atInfo().log(String.format("Applying Service for Gerrit Operator: %s", svc.toString()));
+    client.resource(svc).createOrReplace();
   }
 }
