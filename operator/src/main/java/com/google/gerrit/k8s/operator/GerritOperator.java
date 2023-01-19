e3659e5a1729f0260595ab0fdc2f3922d74e2b2e
Thomas Draebing <thomas.draebing@sap.com>
2023-01-19 13:47:10 +0100

    [Operator] Operator creates its own Service
    To enable access of the HTTP Server used by the operator for cluster
    components, a Service is required.
    
    The operator creates the required Service on startup. This reduces
    the manual configuration admins have to do to set up the operator.
    
    Change-Id: I66ae44abbcb223fde9f95ad6b859e7004391ea01

--- operator/src/main/java/com/google/gerrit/k8s/operator/GerritOperator.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/GerritOperator.java
@@ -14,12 +14,20 @@
 
 package com.google.gerrit.k8s.operator;
 
+import static com.google.gerrit.k8s.operator.server.HttpServer.PORT;
+
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.ServicePort;
+import io.fabric8.kubernetes.api.model.ServicePortBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import java.util.Map;
 import java.util.Set;
 
 @Singleton
@@ -34,15 +42,22 @@ public class GerritOperator {
   @SuppressWarnings("rawtypes")
   private final Set<Reconciler> reconcilers;
 
+  private final String namespace;
+
   private Operator operator;
+  private Service svc;
 
   @Inject
   @SuppressWarnings("rawtypes")
   public GerritOperator(
-      LifecycleManager lifecycleManager, KubernetesClient client, Set<Reconciler> reconcilers) {
+      LifecycleManager lifecycleManager,
+      KubernetesClient client,
+      Set<Reconciler> reconcilers,
+      @Named("Namespace") String namespace) {
     this.lifecycleManager = lifecycleManager;
     this.client = client;
     this.reconcilers = reconcilers;
+    this.namespace = namespace;
   }
 
   public void start() throws Exception {
@@ -53,6 +68,38 @@ public class GerritOperator {
       operator.register(reconciler);
     }
     operator.start();
+    lifecycleManager.addShutdownHook(2, new Thread(this::shutdown));
     lifecycleManager.addShutdownHook(999, new Thread(operator::stop));
+    applyService();
+  }
+
+  public void shutdown() {
+    client.resource(svc).delete();
+  }
+
+  private void applyService() {
+    ServicePort port =
+        new ServicePortBuilder()
+            .withName("http")
+            .withPort(SERVICE_PORT)
+            .withNewTargetPort(PORT)
+            .withProtocol("TCP")
+            .build();
+    svc =
+        new ServiceBuilder()
+            .withApiVersion("v1")
+            .withNewMetadata()
+            .withName(SERVICE_NAME)
+            .withNamespace(namespace)
+            .endMetadata()
+            .withNewSpec()
+            .withType("ClusterIP")
+            .withPorts(port)
+            .withSelector(Map.of("app", "gerrit-operator"))
+            .endSpec()
+            .build();
+
+    logger.atInfo().log(String.format("Applying Service for Gerrit Operator: %s", svc.toString()));
+    client.resource(svc).createOrReplace();
   }
 }
