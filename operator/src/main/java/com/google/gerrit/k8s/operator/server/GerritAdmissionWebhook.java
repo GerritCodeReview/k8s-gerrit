9f1fe2046606567e31eadc0dc491b802d9a29377
Thomas Draebing <thomas.draebing@sap.com>
2023-02-06 16:39:32 +0100

    [Operator] Add validing webhook for Gerrit resources
    The new validating webhook tests, whether a Gerrit Cluster
    is referenced in the GerritSpec and ensures that only one
    primary Gerrit is allowed per Gerrit cluster.
    
    The requirement of a unique primary Gerrit per Gerrit
    cluster comes from the definition of a Gerrit Cluster
    and the resulting network routes. A Gerrit Cluster is
    defined to work on a single set of repositories. In
    case of multiple primary Gerrit's it would be impossible
    to decide how to route requests, e.g. on project basis,
    since not all URLs reference the projects directly.
    
    Change-Id: I1656a2a36cda47fb3b72e7196565ffd7c09313b4

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/server/GerritAdmissionWebhook.java
@@ -0,0 +1,82 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.server;
+
+import com.google.gerrit.k8s.operator.gerrit.Gerrit;
+import com.google.gerrit.k8s.operator.gerrit.GerritSpec.GerritMode;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.Status;
+import io.fabric8.kubernetes.api.model.StatusBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Singleton
+public class GerritAdmissionWebhook extends ValidatingAdmissionWebhookServlet {
+  private static final long serialVersionUID = 1L;
+
+  private final KubernetesClient client;
+
+  @Inject
+  public GerritAdmissionWebhook(KubernetesClient client) {
+    this.client = client;
+  }
+
+  @Override
+  Status validate(HasMetadata resource) {
+    Gerrit gerrit = (Gerrit) resource;
+
+    if (!clusterNameSet(gerrit)) {
+      return new StatusBuilder()
+          .withCode(HttpServletResponse.SC_BAD_REQUEST)
+          .withMessage("Gerrit.spec.cluster is required.")
+          .build();
+    }
+
+    if (!onlyOnePrimaryGerritInCluster(gerrit)) {
+      return new StatusBuilder()
+          .withCode(HttpServletResponse.SC_CONFLICT)
+          .withMessage("Only a single primary Gerrit is allowed per Gerrit Cluster.")
+          .build();
+    }
+
+    return new StatusBuilder().withCode(HttpServletResponse.SC_OK).build();
+  }
+
+  private boolean clusterNameSet(Gerrit gerrit) {
+    String clusterName = gerrit.getSpec().getCluster();
+    return clusterName != null && !clusterName.isBlank();
+  }
+
+  private boolean onlyOnePrimaryGerritInCluster(Gerrit gerrit) {
+    if (gerrit.getSpec().getMode() != GerritMode.PRIMARY) {
+      return true;
+    }
+    String clusterName = gerrit.getSpec().getCluster();
+    return client
+        .resources(Gerrit.class)
+        .inNamespace(gerrit.getMetadata().getNamespace())
+        .list()
+        .getItems()
+        .stream()
+        .noneMatch(g -> clusterName.equals(g.getSpec().getCluster()));
+  }
+
+  @Override
+  public String getName() {
+    return "gerrit";
+  }
+}
