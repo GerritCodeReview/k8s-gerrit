3f35b25387d1bb7df71a0865f0079379cb83930a
Musab Shakeel <mskeel@arista.com>
2023-09-12 15:48:16 -0400

    [Operator] Add support for Ambassador ingress type
    This change adds an Ambassador-based GerritNetworkReconciler, which is
    used when the `INGRESS` environment variable for the operator is set to
    "ambassador". In this case, the precondition is that the Ambassador CRDs
    must already be pre-deployed in the k8s cluster. This change uses the
    CRD version `getambassador.io/v2`.
    
    Ambassador (also known as "Emissary") is an open-source ingress provider
    that sets up ingresses for services via Custom Resources such as
    `Mapping`, `TLSContext`, etc. The newly created
    GerritAmbassadorReconciler creates and manages these resources as josdk
    "dependent resources". The mappings are created to direct trafic to
    Primary Gerrit and/or Replica Gerrit and/or Receivers in the
    GerritCluster. If a GerritCluster has both a Primary and a Replica, then
    all read traffic (git fetch/clone requests) is directed to the Replica,
    and all write traffic (git push) is directed to the Primary.
    
    Because there does not exist a fabric8 extension for Ambassador custom
    resources, managing Ambassador CRs in the operator is a little tricky.
    Two options were considered:
    1. Use fabric8 k8s client's `GenericKubernetesResource` class.
    `GenericKubernetesResource` implements the `HasMetadata` interface, just
    like the `CustomResource` parent class that is used to define custom
    resources like GerritCluster etc. `GenericKubernetesResource` can be
    used to create custom resources by defining the resource spec as a Java
    Map<String, String>. However, with this option, we would need to
    subclass `GenericKubernetesResource` (e.g. `OperatorMappingWrapper`) to
    be able to provide an apiVersion and/or group (expected by josdk). This
    would introduce an unnecessary CRD to the operator, which is not
    desirable.
    2. Generate Ambassador custom resource POJOs from the CRD yaml using
    `java-generator-maven-plugin`. This makes it such that it appears to the
    operator that the Ambassador resources were manually defined in the
    source code as Java classes, just like the other resources -
    GerritCluster, Gerrit, etc.
    
    We went with option 2.
    
    Ambassador CRDs are fetched from
    https://github.com/emissary-ingress/emissary/blob/master/manifests/emissary/emissary-crds.yaml.in
    and stored in the repo as a yaml file. The `java-generator-maven-plugin`
    (fabric8 project) is used to generate POJOs from this CRD yaml. The
    POJOs represent the Ambassador CRs in Java classes.
    
    Manual edits to the Ambassador CRDs
    - The yaml file defines many CRDs but we only need `Mapping` and
    `TLSContext` for this change so the rest of the CRDs are deleted from
    the file
    - The generator plugin has a bug while converting enum types
    (https://github.com/fabric8io/kubernetes-client/issues/5457). To avoid
    hitting this bug, the `v2ExplicitTLS` field in the Mapping CRD
    `v3alpha1` version is commented out
    - Emissary CRD apiVersions are not self-contained. There is a field
    `ambassador_id` in `Mapping` and `TLSContext` CRD that is not defined in
    apiVersion v2 but users are still able to create v2 Mappings with
    ambassador_id field (Emissary converts v2 resources to v3 via webhooks
    defined in emissary service). To be able to define `ambassador_id` in
    the Mapping/TLSContext CRs created via the operator, we manually add
    `ambassador_id` to the v2 Mapping and TLSContext CRD.
    
    Currently, the operator is watching for changes in resources in all
    namespaces. If you have Ambassador resources deployed in your k8s
    cluster that use both v1 and v2 apiVersions, you might run into
    deserialization errors as the operator attempts to deserialize the
    existing v1 resources into the v2 POJOs.
    
    Change-Id: I23d446e21da87e33c71fe3dad481ec34cd963bbe

---
+++ operator/src/test/java/com/google/gerrit/k8s/operator/network/ambassador/dependent/GerritClusterAmbassadorTest.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.network.ambassador.dependent;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.k8s.operator.network.model.GerritNetwork;
+import io.getambassador.v2.Mapping;
+import io.javaoperatorsdk.operator.ReconcilerUtils;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class GerritClusterAmbassadorTest {
+
+  @ParameterizedTest
+  @MethodSource("provideYamlManifests")
+  public void expectedGerritClusterAmbassadorComponentsCreated(
+      String inputFile, Map<String, String> expectedOutputFileNames)
+      throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
+          IllegalAccessException, InvocationTargetException {
+    GerritNetwork gerritNetwork =
+        ReconcilerUtils.loadYaml(GerritNetwork.class, this.getClass(), inputFile);
+
+    for (Map.Entry<String, String> entry : expectedOutputFileNames.entrySet()) {
+      String className = entry.getKey();
+      String expectedOutputFile = entry.getValue();
+
+      Class<?> clazz = Class.forName(className);
+      Object dependentObject = clazz.getDeclaredConstructor(new Class[] {}).newInstance();
+
+      if (dependentObject instanceof MappingDependentResourceInterface) {
+        MappingDependentResourceInterface dependent =
+            (MappingDependentResourceInterface) dependentObject;
+        Mapping result = dependent.desired(gerritNetwork, null);
+        Mapping expected =
+            ReconcilerUtils.loadYaml(Mapping.class, this.getClass(), expectedOutputFile);
+        assertThat(result.getSpec()).isEqualTo(expected.getSpec());
+      }
+    }
+  }
+
+  private static Stream<Arguments> provideYamlManifests() {
+    return Stream.of(
+        Arguments.of(
+            "../../gerritnetwork_primary_replica_tls.yaml",
+            Map.of(
+                GerritClusterMappingGETReplica.class.getName(),
+                    "mappingGETReplica_primary_replica.yaml",
+                GerritClusterMappingPOSTReplica.class.getName(),
+                    "mappingPOSTReplica_primary_replica.yaml",
+                GerritClusterMappingPrimary.class.getName(), "mappingPrimary_primary_replica.yaml"
+                // TODO: add tls
+                )),
+        Arguments.of(
+            "../../gerritnetwork_primary_replica.yaml",
+            Map.of(
+                GerritClusterMappingGETReplica.class.getName(),
+                    "mappingGETReplica_primary_replica.yaml",
+                GerritClusterMappingPOSTReplica.class.getName(),
+                    "mappingPOSTReplica_primary_replica.yaml",
+                GerritClusterMappingPrimary.class.getName(),
+                    "mappingPrimary_primary_replica.yaml")),
+        Arguments.of(
+            "../../gerritnetwork_primary.yaml",
+            Map.of(GerritClusterMapping.class.getName(), "mapping_primary.yaml")),
+        Arguments.of(
+            "../../gerritnetwork_replica.yaml",
+            Map.of(GerritClusterMapping.class.getName(), "mapping_replica.yaml")),
+        Arguments.of(
+            "../../gerritnetwork_receiver_replica.yaml",
+            Map.of(
+                GerritClusterMapping.class.getName(), "mapping_replica.yaml",
+                GerritClusterMappingReceiver.class.getName(), "mapping_receiver.yaml",
+                GerritClusterMappingReceiverGET.class.getName(), "mappingGET_receiver.yaml")),
+        Arguments.of(
+            "../../gerritnetwork_receiver_replica_tls.yaml",
+            Map.of(
+                GerritClusterMapping.class.getName(), "mapping_replica.yaml",
+                GerritClusterMappingReceiver.class.getName(), "mapping_receiver.yaml",
+                GerritClusterMappingReceiverGET.class.getName(), "mappingGET_receiver.yaml"
+                // TODO: add tls
+                )));
+  }
+}
