c8023284ad18574f26ec055f7c5da19ac9f5e1c3
Thomas Draebing <thomas.draebing@sap.com>
2023-10-10 16:00:44 +0200

    [DO NOT MERGE] Update with deletions
    Change-Id: Idfadf7084f878f6d510b6012f4aa3f8f670d13b1

--- operator/src/main/java/com/google/gerrit/k8s/operator/cluster/dependent/SharedPVCCondition.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/cluster/dependent/SharedPVCCondition.java
@@ -14,7 +14,7 @@
 
 package com.google.gerrit.k8s.operator.cluster.dependent;
 
-import com.google.gerrit.k8s.operator.v1beta1.api.model.cluster.GerritCluster;
+import com.google.gerrit.k8s.operator.v1beta2.api.model.cluster.GerritCluster;
 import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
