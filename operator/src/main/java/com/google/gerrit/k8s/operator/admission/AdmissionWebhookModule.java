b9123bd598cee566f26a4529cfb1a661a34b17ce
Thomas Dräbing <thomas.draebing@sap.com>
2023-11-22 09:30:00 +0100

    Remove unused imports
    Change-Id: I9618758b8107ce3b49c0e2ce0161842d572361aa

--- operator/src/main/java/com/google/gerrit/k8s/operator/admission/AdmissionWebhookModule.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/admission/AdmissionWebhookModule.java
@@ -14,9 +14,6 @@
 
 package com.google.gerrit.k8s.operator.admission;
 
-import com.google.gerrit.k8s.operator.v1alpha.admission.GerritClusterValidationWebhookConfigApplier;
-import com.google.gerrit.k8s.operator.v1alpha.admission.GerritValidationWebhookConfigApplier;
-import com.google.gerrit.k8s.operator.v1alpha.admission.GitGcValidationWebhookConfigApplier;
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
