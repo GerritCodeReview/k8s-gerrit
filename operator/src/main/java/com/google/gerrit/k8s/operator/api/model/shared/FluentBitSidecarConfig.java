a62a87938ed082cb139cc74161b9d5a7d949722c
Wendy Wang <wendy.wang10@sap.com>
2024-01-16 16:17:22 +0100

    [Operator] Add Fluent Bit Sidecar for Logging
    Each Gerrit pod will have a fluent bit sidecar attached.
    
    By default the sidecar is configured to simply tail Gerrit log files,
    tag log messages with the file name they are sourced from, and output
    to stdout. New configuration can be provided to the sidecar as a string.
    
    The operator set up the input configuration as that depends on the
    log volume.
    
    Configuration options can be found:
    https://docs.fluentbit.io/manual/administration/configuring-fluent-bit/classic-mode/configuration-file
    
    Change-Id: I468c6b4b18848a07cfc33b1a53d3da3ef5d6a31e

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/api/model/shared/FluentBitSidecarConfig.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.api.model.shared;
+
+public class FluentBitSidecarConfig {
+  private String image = "fluent/fluent-bit:latest";
+  private String config = "";
+  private boolean enabled = false;
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void isEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public String getConfig() {
+    return config;
+  }
+
+  public void setConfig(String config) {
+    this.config = config;
+  }
+}
