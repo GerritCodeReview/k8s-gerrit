7a56f1b487a28223f6ed58361700c266603cc78d
Wendy Wang <wendy.wang10@sap.com>
2024-01-16 16:17:22 +0100

    [Operator] Add Fluent Bit Sidecar for Logging
    Each Gerrit pod will have a fluent bit sidecar attached.
    
    By default the sidecar is configured to simply tail Gerrit log files
    and output to stdout. A new configuration can be provided to the sidecar
    as a string.
    
    The operator is solely responsible to set up the [INPUT] as that is
    dependent on the log volume.
    
    Configuration options can be found:
    https://docs.fluentbit.io/manual/administration/configuring-fluent-bit/classic-mode/configuration-file
    
    Change-Id: I468c6b4b18848a07cfc33b1a53d3da3ef5d6a31e

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/cluster/dependent/FluentBitConfigMap.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.cluster.dependent;
+
+import com.google.gerrit.k8s.operator.v1beta2.api.model.cluster.GerritCluster;
+import com.google.gerrit.k8s.operator.v1beta3.api.model.gerrit.Gerrit;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
+import java.util.Map;
+
+@KubernetesDependent(resourceDiscriminator = FluentBitConfigMapDiscriminator.class)
+public class FluentBitConfigMap extends CRUDKubernetesDependentResource<ConfigMap, Gerrit> {
+
+  public FluentBitConfigMap() {
+    super(ConfigMap.class);
+  }
+
+  public static String getName(Gerrit gerrit) {
+    return String.format("%s-fluentbit-configmap", gerrit.getMetadata().getName());
+  }
+
+  @Override
+  protected ConfigMap desired(Gerrit gerrit, Context<Gerrit> context) {
+    String customConfig = gerrit.getSpec().getFluentBitSidecar().getConfig();
+    String configName = gerrit.getSpec().getFluentBitSidecar().getConfigName();
+    String config = """
+        [INPUT]
+          Name            tail
+          Path            /var/mnt/logs/*log
+          Tag             <log_name>
+          Tag_Regex       logs.(?<log_name>[^*]+)
+        [OUTPUT]
+          Name            stdout
+          Match           *\n
+        """;
+
+    if (customConfig != null) {
+      config = config + customConfig;
+    }
+
+    return new ConfigMapBuilder()
+        .withApiVersion("v1")
+        .withNewMetadata()
+        .withName(getName(gerrit))
+        .withNamespace(gerrit.getMetadata().getNamespace())
+        .withLabels(
+            GerritCluster.getLabels(
+                gerrit.getMetadata().getName(), getName(gerrit), this.getClass().getSimpleName()))
+        .endMetadata()
+        .withData(Map.of(configName, config))
+        .build();
+  }
+}
--- operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/dependent/GerritStatefulSet.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/dependent/GerritStatefulSet.java
@@ -17,12 +17,16 @@ package com.google.gerrit.k8s.operator.gerrit.dependent;
 import static com.google.gerrit.k8s.operator.gerrit.dependent.GerritSecret.CONTEXT_SECRET_VERSION_KEY;
 
 import com.google.common.flogger.FluentLogger;
+<<<<<<< PATCH SET (267093 [Operator] Add Fluent Bit Sidecar for Logging)
+import com.google.gerrit.k8s.operator.cluster.dependent.FluentBitConfigMap;
+=======
 import com.google.gerrit.k8s.operator.api.model.cluster.GerritCluster;
 import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
 import com.google.gerrit.k8s.operator.api.model.gerrit.GerritModule;
 import com.google.gerrit.k8s.operator.api.model.gerrit.GerritModuleData;
 import com.google.gerrit.k8s.operator.api.model.shared.ContainerImageConfig;
 import com.google.gerrit.k8s.operator.api.model.shared.NfsWorkaroundConfig;
+>>>>>>> BASE      (1e7a4c [Operator] Change process to create new CRD version)
 import com.google.gerrit.k8s.operator.gerrit.GerritReconciler;
 import com.google.gerrit.k8s.operator.util.CRUDReconcileAddKubernetesDependentResource;
 import io.fabric8.kubernetes.api.model.Container;
@@ -170,6 +174,13 @@ public class GerritStatefulSet
         .withLivenessProbe(gerrit.getSpec().getLivenessProbe())
         .addAllToVolumeMounts(getVolumeMounts(gerrit, false))
         .endContainer()
+        .addNewContainer()
+        .withName("fluentbit-logger")
+        .withEnv(getEnvVars(gerrit))
+        .withImagePullPolicy(gerrit.getSpec().getContainerImages().getImagePullPolicy())
+        .withImage(gerrit.getSpec().getFluentBitSidecar().getImage())
+        .addAllToVolumeMounts(getFluentbitVolumeMounts(gerrit))
+        .endContainer()
         .addAllToVolumes(getVolumes(gerrit))
         .endSpec()
         .endTemplate()
@@ -229,6 +240,14 @@ public class GerritStatefulSet
             .withName("gerrit-config")
             .withNewConfigMap()
             .withName(GerritConfigMap.getName(gerrit))
+            .endConfigMap()
+            .build());
+
+    volumes.add(
+        new VolumeBuilder()
+            .withName(FluentBitConfigMap.getName(gerrit))
+            .withNewConfigMap()
+            .withName(FluentBitConfigMap.getName(gerrit))
             .endConfigMap()
             .build());
 
@@ -296,7 +315,6 @@ public class GerritStatefulSet
         volumeMounts.add(GerritCluster.getPluginCacheVolumeMount());
       }
     }
-
     for (GerritModule module : gerrit.getSpec().getAllGerritModules()) {
       GerritModuleData md = module.getModuleData();
       if (md == null) {
@@ -314,6 +332,19 @@ public class GerritStatefulSet
     if (nfsWorkaround.isEnabled() && nfsWorkaround.getIdmapdConfig() != null) {
       volumeMounts.add(GerritCluster.getNfsImapdConfigVolumeMount());
     }
+
+    return volumeMounts;
+  }
+
+  private Set<VolumeMount> getFluentbitVolumeMounts(Gerrit gerrit) {
+    Set<VolumeMount> volumeMounts = new HashSet<>();
+    volumeMounts.add(
+        new VolumeMountBuilder()
+            .withName(FluentBitConfigMap.getName(gerrit))
+            .withMountPath("/fluent-bit/etc/")
+            .build());
+
+    volumeMounts.add(GerritCluster.getLogsVolumeMount());
 
     return volumeMounts;
   }
