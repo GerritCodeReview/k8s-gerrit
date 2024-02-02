fa4b574e9b846a0f863559a22787d036fac2dd5e
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-02-02 13:08:38 +0100

    Provision a gerrit cluster without nfs
    This change allows the k8s-gerrit operator to deal with multiple
    replicas in the same stateful set with or without having a shared
    file system based on the feature flag introduced in change 409743.
    
    When the cluster mode is set to `MULTISITE`, each K8s replica
    will have its own persistent volume to store the gerrit site.
    
    Change-Id: Ia07c495ef1d3e3dad349a333e05625f6bea73487

--- container-images/gerrit-init/tools/gerrit-initializer/initializer/config/init_config.py
+++ container-images/gerrit-init/tools/gerrit-initializer/initializer/config/init_config.py
@@ -16,6 +16,8 @@ import os.path
 
 import yaml
 
+from .cluster_mode import ClusterMode
+
 
 class InitConfig:
     def __init__(self):
@@ -28,6 +30,7 @@ class InitConfig:
 
         self.is_ha = False
         self.refdb = False
+        self.cluster_mode = ClusterMode.HIGH_AVAILABILITY
 
     def parse(self, config_file):
         if not os.path.exists(config_file):
@@ -55,6 +58,10 @@ class InitConfig:
             self.ca_cert_path = config["caCertPath"]
 
         self.is_ha = "highAvailability" in config and config["highAvailability"]
+
+        if "clusterMode" in config:
+            self.cluster_mode = config["clusterMode"]
+
         if "refdb" in config:
             self.refdb = config["refdb"]
 
