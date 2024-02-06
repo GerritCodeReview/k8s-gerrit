d6e038c30fc7c0c42a0980fc703582ffb4d06f8a
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-02-06 18:52:40 +0100

    Expand gerrit-init to set up pull replication
    When using pull replication gerrit-init container
    is setting up the remotes for pull replication.
    
    To do this a new step in the initializer has been
    added that:
    
    - check if pull-replication needs to be used
    - create the specific configuration for each
      pod in the statefulset leveraging the pod-index label
    
    Please note that the configmaps are immutable so
    the PullReplicationConfigurator copies them in the
    filesystem and use them as a template for setting
    replication.config.
    
    At the moment PullReplicationConfigurator does not
    support auto scaling.
    
    Change-Id: I8bbb4ccd47ce2a65eff5bf6c59b655e0ae53d860

---
+++ container-images/gerrit-init/tools/gerrit-initializer/initializer/tasks/pull_replication_configurator.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ..helpers import log
+
+LOG = log.get_logger("init")
+MNT_PATH = "/var/mnt"
+INSTANCE_ID_PLACEHOLDER = "INSTANCE_ID_PLACEHOLDER"
+REMOTE_ID_PLACEHOLDER = "REMOTE_INSTANCE_ID_PLACEHOLDER"
+REMOTE_HOST_PLACEHOLDER = "REMOTE_INSTANCE_HOST_PLACEHOLDER"
+
+
+class PullReplicationConfigurator:
+    def __init__(self, site):
+        self.site = site
+        self.pod_id = self._get_pod_identifier()
+        self.remote_host_postfix = self._get_remote_host_postfix()
+        self.replicas = self._get_replicas_num()
+
+    @staticmethod
+    def has_pull_replication():
+        return os.path.exists(
+            os.path.join(MNT_PATH, "etc/config/replication.config")
+        ) and (
+            os.path.exists("/var/gerrit/lib/pull-replication.jar")
+            or os.path.exists("/var/gerrit/plugins/pull-replication.jar")
+        )
+
+    def _get_pod_identifier(self):
+        return int(os.environ.get("POD_INDEX"))
+
+    def _get_replicas_num(self):
+        return int(os.environ.get("REPLICAS"))
+
+    def _get_remote_host_postfix(self):
+        namespace = os.environ.get("NAMESPACE_NAME")
+        headless_svc = os.environ.get("HEADLESS_SVC")
+        return f"{headless_svc}.{namespace}.svc.cluster.local"
+
+    def _configure_instance_id(self, template_path):
+        with open(template_path, "r") as file:
+            content = file.read()
+        content = content.replace(f"{INSTANCE_ID_PLACEHOLDER}", f"gerrit-{self.pod_id}")
+        with open(os.path.join(self.site, "etc/gerrit.config"), "w") as file:
+            file.write(content)
+
+    def _configure_remotes(self, template_path):
+        with open(template_path, "r") as file:
+            content = file.read()
+        remote_pod_ids = list(range(0, self.replicas))
+        del remote_pod_ids[self.pod_id]
+        for idx, x in enumerate(remote_pod_ids):
+            content = content.replace(
+                f"{REMOTE_ID_PLACEHOLDER}-{idx + 1}", f"gerrit-{x}"
+            )
+            content = content.replace(
+                f"{REMOTE_HOST_PLACEHOLDER}-{idx + 1}",
+                f"gerrit-{x}.{self.remote_host_postfix}",
+            )
+            LOG.info('Set pull replication for remote "{}"'.format(f"gerrit-{x}"))
+        with open(os.path.join(self.site, "etc/replication.config"), "w") as file:
+            file.write(content)
+
+    def configure(self):
+        LOG.info(
+            "Setting pull replication configuration for pod-idx: {}".format(self.pod_id)
+        )
+
+        replication_config_configmap = os.path.join(
+            MNT_PATH, "etc/config/replication.config"
+        )
+        gerrit_config_configmap = os.path.join(MNT_PATH, "etc/config/gerrit.config")
+
+        self._configure_remotes(replication_config_configmap)
+        self._configure_instance_id(gerrit_config_configmap)
