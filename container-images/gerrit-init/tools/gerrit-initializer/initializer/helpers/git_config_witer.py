639efe85b86571b21987b5b77042045abd3e148a
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-02-06 18:52:40 +0100

    Expand gerrit-init to set up pull replication
    When using pull replication gerrit-init container
    is setting up the remotes for pull replication.
    
    To do this a new step in the initializer has been
    added that:
    
    - check if pull-replication needs to be used
    - create the specific configuration for each
      pod in the statefulset
    
    The pod identifier in the statefulset is derived
    from the `pod-name` already available in the operator.
    
    Please note that the configmaps are immutable so
    the PullReplicationConfigurator reads the common options
    specified for the remotes and use them to populate each
    remote section in replication.config.
    
    Change-Id: I8bbb4ccd47ce2a65eff5bf6c59b655e0ae53d860

---
+++ container-images/gerrit-init/tools/gerrit-initializer/initializer/helpers/git_config_witer.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+
+class GitConfigWriter:
+    def __init__(self, options):
+        self.options = options
+
+    def remove_subsection(self, section, subsection):
+        """
+        Filter all the options defined in git config file,
+        returning only the options that match the section specified.
+        """
+        self.options = list(
+            filter(
+                lambda tag: not (
+                    tag["section"] == section and tag["subsection"] == subsection
+                ),
+                self.options,
+            )
+        )
+
+    def write_config(self, path):
+        """
+        Write the options as a git config file.
+        """
+        with open(path, "w") as file:
+            file.write(self._get_config_as_string())
+
+    def _list_section(self, section, subsection):
+        """
+        Filter all the options defined in git config file,
+        returning only the options that match the section specified.
+        """
+        if subsection is not None:
+            return list(
+                filter(
+                    lambda tag: tag["section"] == section
+                    and tag["subsection"] == subsection,
+                    self.options,
+                )
+            )
+        else:
+            return list(filter(lambda tag: tag["section"] == section, self.options))
+
+    def _get_config_as_string(self):
+        """
+        Return a string representation of the options as written in a git config file.
+        """
+        cfg_str = ""
+        for section in self._get_all_section_names():
+            cfg_str += self._get_section_as_string(
+                section["section"], section.get("subsection", None)
+            )
+        return cfg_str
+
+    def _get_all_section_names(self):
+        sections = []
+        for x in self.options:
+            if x["subsection"] is not None:
+                sections.append(
+                    {"section": x["section"], "subsection": x["subsection"]}
+                )
+            else:
+                sections.append({"section": x["section"]})
+        section_names = set(json.dumps(i, sort_keys=True) for i in sections)
+        return map(lambda s: json.loads(s), section_names)
+
+    def _get_section_as_string(self, section, subsection):
+        section_options = self._list_section(section, subsection)
+        options_str = "\n\t".join(
+            [f"{item['key']} = {item['value']}" for item in section_options]
+        )
+        output_section = section
+        if subsection is not None:
+            output_section += f' "{subsection}"'
+        return f"[{output_section}]\n\t{options_str}\n"
---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/config/PullReplicationPluginConfigBuilder.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.gerrit.config;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
+import com.google.gerrit.k8s.operator.gerrit.dependent.GerritHeadlessService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ConfigConstants;
+
+public class PullReplicationPluginConfigBuilder extends ConfigBuilder {
+
+  public PullReplicationPluginConfigBuilder(Gerrit gerrit) {
+    super(
+        gerrit.getSpec().getConfigFiles().getOrDefault("replication.config", ""),
+        ImmutableList.copyOf(collectRequiredOptions(gerrit)));
+  }
+
+  private static List<RequiredOption<?>> collectRequiredOptions(Gerrit gerrit) {
+    List<RequiredOption<?>> requiredOptions = new ArrayList<>();
+    requiredOptions.add(new RequiredOption<Boolean>("replication", "consumeStreamEvents", false));
+    requiredOptions.add(new RequiredOption<String>("replication", "syncRefs", "ALL REFS ASYNC"));
+    return requiredOptions;
+  }
+
+  public Config makeRemoteSections(Config config, Gerrit gerrit) {
+    String headlessServiceHostname = new GerritHeadlessService().getHostname(gerrit);
+    for (int i = 0; i < gerrit.getSpec().getReplicas(); i++) {
+      buildRemoteSection(config, "gerrit-" + i, headlessServiceHostname);
+    }
+    config.unsetSection(ConfigConstants.CONFIG_REMOTE_SECTION, null);
+    return config;
+  }
+
+  private static void buildRemoteSection(
+      Config config, String newRemoteName, String headlessServiceHostName) {
+    String remoteSection = ConfigConstants.CONFIG_REMOTE_SECTION;
+
+    // Set url and apiUrl for the remote section
+    config.setString(
+        remoteSection,
+        newRemoteName,
+        "url",
+        "http://" + newRemoteName + "." + headlessServiceHostName + ":8080/${name}.git");
+    config.setString(
+        remoteSection,
+        newRemoteName,
+        "apiUrl",
+        "http://" + newRemoteName + "." + headlessServiceHostName + ":8080");
+
+    // Get all keys in the original [remote] section
+    Set<String> keys = config.getNames(remoteSection, true);
+
+    // Copy each key-value pair to the new remote section
+    for (String key : keys) {
+      String value = config.getString(remoteSection, null, key);
+      config.setString(remoteSection, newRemoteName, key, value);
+    }
+  }
+}
