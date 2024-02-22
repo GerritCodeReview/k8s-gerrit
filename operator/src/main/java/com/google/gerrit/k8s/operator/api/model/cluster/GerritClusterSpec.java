6ef175ab6ab2ee35b9c24eb0d04e544b4cc133a3
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-02-22 14:00:39 +0100

    Use pull-replication plugin standalone with a broker
    This change configures the pull-replication plugin [1] to consume events
    from Kafka broker [2]. To achieve this, events-kafka [3] plugin is
    installed as an implementation of events-broker [4]. This enhancement
    enables the provision of a backfill mechanism, allowing nodes to catch
    up with events after periods of unreachability.
    
    References:
    [1] https://gerrit.googlesource.com/plugins/pull-replication/+/refs/heads/stable-3.9/src/main/resources/Documentation/config.md#file-2
    [2] https://kafka.apache.org/
    [3] https://gerrit.googlesource.com/plugins/events-kafka/+/refs/heads/stable-3.9/src/main/resources/Documentation/config.md#additional-properties
    [4] https://gerrit.googlesource.com/modules/events-broker/+/refs/heads/stable-3.9
    
    Change-Id: I28e831cfc16cc4d746562c8dfd1e5e5ee8cfe691

--- operator/src/main/java/com/google/gerrit/k8s/operator/api/model/cluster/GerritClusterSpec.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/api/model/cluster/GerritClusterSpec.java
@@ -16,11 +16,7 @@ package com.google.gerrit.k8s.operator.api.model.cluster;
 
 import com.google.gerrit.k8s.operator.api.model.gerrit.GerritTemplate;
 import com.google.gerrit.k8s.operator.api.model.receiver.ReceiverTemplate;
-import com.google.gerrit.k8s.operator.api.model.shared.ContainerImageConfig;
-import com.google.gerrit.k8s.operator.api.model.shared.FluentBitSidecarConfig;
-import com.google.gerrit.k8s.operator.api.model.shared.GerritClusterIngressConfig;
-import com.google.gerrit.k8s.operator.api.model.shared.GerritStorageConfig;
-import com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig;
+import com.google.gerrit.k8s.operator.api.model.shared.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -31,6 +27,7 @@ public class GerritClusterSpec {
   private ContainerImageConfig containerImages = new ContainerImageConfig();
   private GerritClusterIngressConfig ingress = new GerritClusterIngressConfig();
   private GlobalRefDbConfig refdb = new GlobalRefDbConfig();
+  private EventsBrokerConfig eventsBroker = new EventsBrokerConfig();
   private String serverId = "";
   private List<GerritTemplate> gerrits = new ArrayList<>();
   private ReceiverTemplate receiver;
@@ -67,6 +64,14 @@ public class GerritClusterSpec {
 
   public void setRefdb(GlobalRefDbConfig refdb) {
     this.refdb = refdb;
+  }
+
+  public EventsBrokerConfig getEventsBroker() {
+    return eventsBroker;
+  }
+
+  public void setEventsBroker(EventsBrokerConfig eventsBroker) {
+    this.eventsBroker = eventsBroker;
   }
 
   public String getServerId() {
@@ -118,6 +123,7 @@ public class GerritClusterSpec {
         ingress,
         receiver,
         refdb,
+        eventsBroker,
         scheduledTasks,
         serverId,
         storage);
@@ -135,6 +141,7 @@ public class GerritClusterSpec {
         && Objects.equals(ingress, other.ingress)
         && Objects.equals(receiver, other.receiver)
         && Objects.equals(refdb, other.refdb)
+        && Objects.equals(eventsBroker, other.eventsBroker)
         && Objects.equals(scheduledTasks, other.scheduledTasks)
         && Objects.equals(serverId, other.serverId)
         && Objects.equals(storage, other.storage);
@@ -150,6 +157,8 @@ public class GerritClusterSpec {
         + ingress
         + ", refdb="
         + refdb
+        + ", eventsBroker="
+        + eventsBroker
         + ", serverId="
         + serverId
         + ", gerrits="
