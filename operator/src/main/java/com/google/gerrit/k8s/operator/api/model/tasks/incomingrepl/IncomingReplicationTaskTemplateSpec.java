cd5e3fbcd30a36488a36ebae4c3749b7bc47211a
Thomas Dräbing <thomas.draebing@sap.com>
2024-02-28 09:20:45 +0100

    WIP Create CronJob for incoming replication
    Change-Id: If886d7d40eace228e47f46e0a4a07d1975a69961

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/api/model/tasks/incomingrepl/IncomingReplicationTaskTemplateSpec.java
@@ -0,0 +1,126 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.api.model.tasks.incomingrepl;
+
+import io.fabric8.generator.annotation.Required;
+import io.fabric8.kubernetes.api.model.Affinity;
+import io.fabric8.kubernetes.api.model.ResourceRequirements;
+import io.fabric8.kubernetes.api.model.Toleration;
+import java.util.List;
+import java.util.Objects;
+
+public class IncomingReplicationTaskTemplateSpec {
+  @Required private String schedule;
+  @Required private IncomingReplicationConfig config;
+  private ResourceRequirements resources;
+  private List<Toleration> tolerations;
+  private Affinity affinity;
+  private String secretRef;
+
+  public IncomingReplicationTaskTemplateSpec() {}
+
+  public IncomingReplicationTaskTemplateSpec(IncomingReplicationTaskTemplateSpec spec) {
+    this.schedule = spec.getSchedule();
+    this.config = spec.getConfig();
+    this.resources = spec.getResources();
+    this.tolerations = spec.getTolerations();
+    this.affinity = spec.getAffinity();
+    this.secretRef = spec.getSecretRef();
+  }
+
+  public String getSchedule() {
+    return schedule;
+  }
+
+  public void setSchedule(String schedule) {
+    this.schedule = schedule;
+  }
+
+  public IncomingReplicationConfig getConfig() {
+    return config;
+  }
+
+  public void setConfig(IncomingReplicationConfig config) {
+    this.config = config;
+  }
+
+  public ResourceRequirements getResources() {
+    return resources;
+  }
+
+  public void setResources(ResourceRequirements resources) {
+    this.resources = resources;
+  }
+
+  public List<Toleration> getTolerations() {
+    return tolerations;
+  }
+
+  public void setTolerations(List<Toleration> tolerations) {
+    this.tolerations = tolerations;
+  }
+
+  public Affinity getAffinity() {
+    return affinity;
+  }
+
+  public void setAffinity(Affinity affinity) {
+    this.affinity = affinity;
+  }
+
+  public String getSecretRef() {
+    return secretRef;
+  }
+
+  public void setSecretRef(String secretRef) {
+    this.secretRef = secretRef;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(affinity, config, resources, schedule, tolerations, secretRef);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    IncomingReplicationTaskTemplateSpec other = (IncomingReplicationTaskTemplateSpec) obj;
+    return Objects.equals(affinity, other.affinity)
+        && Objects.equals(config, other.config)
+        && Objects.equals(resources, other.resources)
+        && Objects.equals(schedule, other.schedule)
+        && Objects.equals(tolerations, other.tolerations)
+        && Objects.equals(secretRef, other.secretRef);
+  }
+
+  @Override
+  public String toString() {
+    return "IncomingReplicationTaskSpec [schedule="
+        + schedule
+        + ", remote="
+        + config
+        + ", resources="
+        + resources
+        + ", tolerations="
+        + tolerations
+        + ", affinity="
+        + affinity
+        + ", secretRef="
+        + secretRef
+        + "]";
+  }
+}
---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/tasks/incomingrepl/IncomingReplicationTaskReconciler.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.tasks.incomingrepl;
+
+import com.google.gerrit.k8s.operator.api.model.tasks.incomingrepl.IncomingReplicationTask;
+import com.google.gerrit.k8s.operator.tasks.incomingrepl.dependent.IncomingReplicationTaskConfigMap;
+import com.google.gerrit.k8s.operator.tasks.incomingrepl.dependent.IncomingReplicationTaskCronJob;
+import com.google.inject.Singleton;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+
+@Singleton
+@ControllerConfiguration(
+    dependents = {
+      @Dependent(
+          name = "incoming-repl-task-configmap",
+          type = IncomingReplicationTaskConfigMap.class),
+      @Dependent(
+          name = "incoming-repl-task-cronjob",
+          type = IncomingReplicationTaskCronJob.class,
+          dependsOn = "incoming-repl-task-configmap")
+    })
+public class IncomingReplicationTaskReconciler implements Reconciler<IncomingReplicationTask> {
+
+  @Override
+  public UpdateControl<IncomingReplicationTask> reconcile(
+      IncomingReplicationTask incomingRepl, Context<IncomingReplicationTask> context) {
+    return UpdateControl.noUpdate();
+  }
+}
