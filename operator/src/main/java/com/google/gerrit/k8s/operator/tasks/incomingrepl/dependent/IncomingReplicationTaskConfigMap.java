8fef1a048720595c5c51c1fc71629df8dea93a94
Thomas Dräbing <thomas.draebing@sap.com>
2024-02-28 09:20:45 +0100

    Create CronJob for fetching remote repositories
    This change adds a new Reconciler that will create a CronJob that
    can be used to regularly fetch projects from remote git servers,
    which do not have to be Gerrit servers.
    
    Change-Id: If886d7d40eace228e47f46e0a4a07d1975a69961

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/tasks/incomingrepl/dependent/IncomingReplicationTaskConfigMap.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.tasks.incomingrepl.dependent;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.gerrit.k8s.operator.api.model.cluster.GerritCluster;
+import com.google.gerrit.k8s.operator.api.model.tasks.incomingrepl.IncomingReplicationTask;
+import com.google.gerrit.k8s.operator.util.CRUDReconcileAddKubernetesDependentResource;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.javaoperatorsdk.operator.OperatorException;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+@KubernetesDependent
+public class IncomingReplicationTaskConfigMap
+    extends CRUDReconcileAddKubernetesDependentResource<ConfigMap, IncomingReplicationTask> {
+  public static final String CONFIG_FILE_NAME = "incoming-replication.config.yaml";
+
+  public IncomingReplicationTaskConfigMap() {
+    super(ConfigMap.class);
+  }
+
+  @Override
+  public ConfigMap desired(
+      IncomingReplicationTask incomingReplTask, Context<IncomingReplicationTask> context) {
+    Map<String, String> labels =
+        GerritCluster.getLabels(
+            incomingReplTask.getMetadata().getName(),
+            getName(incomingReplTask),
+            this.getClass().getSimpleName());
+
+    return new ConfigMapBuilder()
+        .withApiVersion("v1")
+        .withNewMetadata()
+        .withName(getName(incomingReplTask))
+        .withNamespace(incomingReplTask.getMetadata().getNamespace())
+        .withLabels(labels)
+        .endMetadata()
+        .withData(Map.of(CONFIG_FILE_NAME, getConfigAsString(incomingReplTask)))
+        .build();
+  }
+
+  public static String getName(IncomingReplicationTask incomingReplTask) {
+    return String.format("%s-configmap", incomingReplTask.getMetadata().getName());
+  }
+
+  private String getConfigAsString(IncomingReplicationTask incomingReplTask) {
+    StringWriter writer = new StringWriter();
+    try {
+      new ObjectMapper(new YAMLFactory())
+          .writeValue(writer, incomingReplTask.getSpec().getConfig());
+    } catch (IOException e) {
+      throw new OperatorException(
+          "Failed to write configuration for incoming replication task.", e);
+    }
+    return writer.toString();
+  }
+}
---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/tasks/incomingrepl/dependent/IncomingReplicationTaskCronJob.java
@@ -0,0 +1,234 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.tasks.incomingrepl.dependent;
+
+import static com.google.gerrit.k8s.operator.tasks.incomingrepl.dependent.IncomingReplicationTaskConfigMap.CONFIG_FILE_NAME;
+
+import com.google.gerrit.k8s.operator.api.model.cluster.GerritCluster;
+import com.google.gerrit.k8s.operator.api.model.shared.ContainerImageConfig;
+import com.google.gerrit.k8s.operator.api.model.shared.NfsWorkaroundConfig;
+import com.google.gerrit.k8s.operator.api.model.tasks.incomingrepl.IncomingReplicationTask;
+import com.google.gerrit.k8s.operator.tasks.incomingrepl.IncomingReplicationTaskReconciler;
+import com.google.gerrit.k8s.operator.util.CRUDReconcileAddKubernetesDependentResource;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerBuilder;
+import io.fabric8.kubernetes.api.model.Secret;
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeBuilder;
+import io.fabric8.kubernetes.api.model.VolumeMount;
+import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
+import io.fabric8.kubernetes.api.model.batch.v1.CronJob;
+import io.fabric8.kubernetes.api.model.batch.v1.CronJobBuilder;
+import io.fabric8.kubernetes.api.model.batch.v1.JobTemplateSpec;
+import io.fabric8.kubernetes.api.model.batch.v1.JobTemplateSpecBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class IncomingReplicationTaskCronJob
+    extends CRUDReconcileAddKubernetesDependentResource<CronJob, IncomingReplicationTask> {
+  private static final String CONFIGMAP_VOLUME_NAME = "incoming-repl-config";
+
+  public IncomingReplicationTaskCronJob() {
+    super(CronJob.class);
+  }
+
+  @Override
+  protected CronJob desired(
+      IncomingReplicationTask incomingReplTask, Context<IncomingReplicationTask> context) {
+    String ns = incomingReplTask.getMetadata().getNamespace();
+    String name = incomingReplTask.getMetadata().getName();
+    Map<String, String> labels = getLabels(incomingReplTask);
+
+    List<Container> initContainers = new ArrayList<>();
+    NfsWorkaroundConfig nfsWorkaround =
+        incomingReplTask.getSpec().getStorage().getStorageClasses().getNfsWorkaround();
+    if (nfsWorkaround.isEnabled() && nfsWorkaround.isChownOnStartup()) {
+      boolean hasIdmapdConfig =
+          incomingReplTask
+                  .getSpec()
+                  .getStorage()
+                  .getStorageClasses()
+                  .getNfsWorkaround()
+                  .getIdmapdConfig()
+              != null;
+      ContainerImageConfig images = incomingReplTask.getSpec().getContainerImages();
+
+      initContainers.add(GerritCluster.createNfsInitContainer(hasIdmapdConfig, images));
+    }
+
+    JobTemplateSpec gitGcJobTemplate =
+        new JobTemplateSpecBuilder()
+            .withNewSpec()
+            .withNewTemplate()
+            .withNewMetadata()
+            .withAnnotations(
+                Map.of(
+                    "sidecar.istio.io/inject",
+                    "false",
+                    "cluster-autoscaler.kubernetes.io/safe-to-evict",
+                    "false"))
+            .withLabels(labels)
+            .endMetadata()
+            .withNewSpec()
+            .withTolerations(incomingReplTask.getSpec().getTolerations())
+            .withAffinity(incomingReplTask.getSpec().getAffinity())
+            .addAllToImagePullSecrets(
+                incomingReplTask.getSpec().getContainerImages().getImagePullSecrets())
+            .withRestartPolicy("OnFailure")
+            .withNewSecurityContext()
+            .withFsGroup(100L)
+            .endSecurityContext()
+            .addAllToInitContainers(initContainers)
+            .addToContainers(buildTaskContainer(incomingReplTask))
+            .withVolumes(getVolumes(incomingReplTask))
+            .endSpec()
+            .endTemplate()
+            .endSpec()
+            .build();
+
+    return new CronJobBuilder()
+        .withApiVersion("batch/v1")
+        .withNewMetadata()
+        .withNamespace(ns)
+        .withName(name)
+        .withLabels(labels)
+        .endMetadata()
+        .withNewSpec()
+        .withSchedule(incomingReplTask.getSpec().getSchedule())
+        .withConcurrencyPolicy("Forbid")
+        .withJobTemplate(gitGcJobTemplate)
+        .endSpec()
+        .build();
+  }
+
+  private static String getComponentName(String incomingReplTaskName) {
+    return String.format("task-incoming-replication-%s", incomingReplTaskName);
+  }
+
+  private static Map<String, String> getLabels(IncomingReplicationTask incomingReplTask) {
+    String name = incomingReplTask.getMetadata().getName();
+    return GerritCluster.getLabels(
+        name, getComponentName(name), IncomingReplicationTaskReconciler.class.getSimpleName());
+  }
+
+  private Container buildTaskContainer(IncomingReplicationTask incomingReplTask) {
+
+    ContainerBuilder taskContainerBuilder =
+        new ContainerBuilder()
+            .withName("incoming-replication")
+            .withImagePullPolicy(
+                incomingReplTask.getSpec().getContainerImages().getImagePullPolicy())
+            .withImage(
+                incomingReplTask
+                    .getSpec()
+                    .getContainerImages()
+                    .getGerritImages()
+                    .getFullImageName("fetch-job"))
+            .withResources(incomingReplTask.getSpec().getResources())
+            .withVolumeMounts(getVolumeMounts(incomingReplTask));
+
+    return taskContainerBuilder.build();
+  }
+
+  private List<VolumeMount> getVolumeMounts(IncomingReplicationTask incomingReplTask) {
+    List<VolumeMount> volumeMounts = new ArrayList<>();
+    volumeMounts.add(GerritCluster.getGitRepositoriesVolumeMount("/var/gerrit/git"));
+
+    volumeMounts.add(
+        new VolumeMountBuilder()
+            .withName(CONFIGMAP_VOLUME_NAME)
+            .withSubPath(CONFIG_FILE_NAME)
+            .withMountPath("/var/gerrit/etc/" + CONFIG_FILE_NAME)
+            .build());
+
+    if (incomingReplTask.getSpec().getStorage().getStorageClasses().getNfsWorkaround().isEnabled()
+        && incomingReplTask
+                .getSpec()
+                .getStorage()
+                .getStorageClasses()
+                .getNfsWorkaround()
+                .getIdmapdConfig()
+            != null) {
+      volumeMounts.add(GerritCluster.getNfsImapdConfigVolumeMount());
+    }
+
+    String secretRef = incomingReplTask.getSpec().getSecretRef();
+    if (secretRef != null && !secretRef.isBlank()) {
+      Secret secret =
+          client
+              .resources(Secret.class)
+              .inNamespace(incomingReplTask.getMetadata().getNamespace())
+              .withName(secretRef)
+              .get();
+      if (secret != null && secret.getData().containsKey(".netrc")) {
+        volumeMounts.add(
+            new VolumeMountBuilder()
+                .withName(secretRef)
+                .withSubPath(".netrc")
+                .withMountPath("/home/gerrit/.netrc")
+                .build());
+      }
+    }
+    return volumeMounts;
+  }
+
+  private List<Volume> getVolumes(IncomingReplicationTask incomingReplTask) {
+    List<Volume> volumes = new ArrayList<>();
+
+    volumes.add(
+        GerritCluster.getSharedVolume(
+            incomingReplTask.getSpec().getStorage().getSharedStorage().getExternalPVC()));
+
+    volumes.add(
+        new VolumeBuilder()
+            .withName(CONFIGMAP_VOLUME_NAME)
+            .withNewConfigMap()
+            .withName(IncomingReplicationTaskConfigMap.getName(incomingReplTask))
+            .endConfigMap()
+            .build());
+
+    if (incomingReplTask
+        .getSpec()
+        .getStorage()
+        .getStorageClasses()
+        .getNfsWorkaround()
+        .isEnabled()) {
+      if (incomingReplTask
+              .getSpec()
+              .getStorage()
+              .getStorageClasses()
+              .getNfsWorkaround()
+              .getIdmapdConfig()
+          != null) {
+        volumes.add(GerritCluster.getNfsImapdConfigVolume());
+      }
+    }
+
+    String secretRef = incomingReplTask.getSpec().getSecretRef();
+    if (secretRef != null && !secretRef.isBlank()) {
+      volumes.add(
+          new VolumeBuilder()
+              .withName(secretRef)
+              .withNewSecret()
+              .withSecretName(secretRef)
+              .endSecret()
+              .build());
+    }
+
+    return volumes;
+  }
+}
