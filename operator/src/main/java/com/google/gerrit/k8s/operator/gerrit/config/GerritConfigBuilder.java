79810208520c3d0ee90a6b9450cf13614ec532c8
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2024-03-13 14:40:43 +0000

    Add multi-site setup into the Gerrit installation
    This change provides a multi-site setup and installs and configures
    the following modules and plugins:
    
    - events-broker [1]
    - events-kafka [2]
    - websession-broker [3]
    - multi-site [4]
    
    The module global-refdb [5] and the plugins zookeeper-refdb [6]
    and spanner-refdb [7] are available in the current code.
    
    References:
    
    [1] https://gerrit.googlesource.com/modules/events-broker/+/refs/heads/stable-3.9
    [2] https://gerrit.googlesource.com/plugins/events-kafka/+/refs/heads/stable-3.9
    [3] https://gerrit.googlesource.com/plugins/websession-broker/+/refs/heads/stable-3.9
    [4] https://gerrit.googlesource.com/plugins/multi-site/+/refs/heads/stable-3.9
    [5] https://gerrit.googlesource.com/modules/global-refdb/+/refs/heads/stable-3.9
    [6] https://gerrit.googlesource.com/plugins/zookeeper-refdb/+/refs/heads/stable-3.9
    [7] https://gerrit.googlesource.com/plugins/spanner-refdb/
    
    Change-Id: I7db984322d58fb0c9eec4ff6ccdbe94a28e0ec11

--- operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/config/GerritConfigBuilder.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/config/GerritConfigBuilder.java
@@ -18,6 +18,8 @@ import static com.google.gerrit.k8s.operator.gerrit.dependent.GerritStatefulSet.
 import static com.google.gerrit.k8s.operator.gerrit.dependent.GerritStatefulSet.SSH_PORT;
 
 import com.google.common.collect.ImmutableList;
+import com.google.gerrit.k8s.operator.Constants;
+import com.google.gerrit.k8s.operator.OperatorContext;
 import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
 import com.google.gerrit.k8s.operator.api.model.gerrit.GerritTemplateSpec.GerritMode;
 import com.google.gerrit.k8s.operator.api.model.shared.EventsBrokerConfig;
@@ -76,8 +78,9 @@ public class GerritConfigBuilder extends ConfigBuilder {
       requiredOptions.add(new RequiredOption<String>("gerrit", "serverId", serverId));
     }
 
-    if ((!gerrit.getSpec().getRefdb().getDatabase().equals(RefDatabase.NONE)
-            && gerrit.getSpec().getMode().equals(GerritMode.PRIMARY))
+    if (OperatorContext.getClusterMode() != Constants.ClusterMode.MULTISITE
+            && (!gerrit.getSpec().getRefdb().getDatabase().equals(RefDatabase.NONE)
+                && gerrit.getSpec().getMode().equals(GerritMode.PRIMARY))
         || gerrit.getSpec().isHighlyAvailablePrimary()) {
       requiredOptions.add(
           new RequiredOption<Set<String>>(
@@ -195,7 +198,7 @@ public class GerritConfigBuilder extends ConfigBuilder {
               "bootstrapServers",
               gerrit.getSpec().getEventsBroker().getKafkaConfig().getConnectString()));
       requiredOptions.add(
-          new RequiredOption<String>("plugin", "events-kafka", "numberOfSubscribers", "6"));
+          new RequiredOption<String>("plugin", "events-kafka", "numberOfSubscribers", "7"));
       requiredOptions.add(
           new RequiredOption<String>("plugin", "events-kafka", "securityProtocol", "PLAINTEXT"));
       requiredOptions.add(
@@ -208,6 +211,9 @@ public class GerritConfigBuilder extends ConfigBuilder {
           new RequiredOption<String>("plugin", "events-kafka", "autoOffsetReset", "latest"));
       requiredOptions.add(
           new RequiredOption<String>("plugin", "events-kafka", "sendStreamEvents", "true"));
+      requiredOptions.add(
+          new RequiredOption<String>(
+              "plugin", "events-kafka", "groupId", "INSTANCE_ID_PLACEHOLDER"));
     }
     return requiredOptions;
   }
