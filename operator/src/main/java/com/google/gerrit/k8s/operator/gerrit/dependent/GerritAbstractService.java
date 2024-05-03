2d13007956e2e20ff9a5453ec5ea3c497f3c731d
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2024-05-03 17:08:53 +0100

    Deploys a headless service as dependency resource in GerritReconciler
    This change deploys a headless service [1] in conjunction with Gerrit's
    StatefulSet to enable direct network communication with individual Pods
    under its management. Unlike conventional services, a headless service
    refrains from assigning a cluster-internal IP to each Pod, opting instead
    to establish DNS records for their respective hostnames.
    
    References:
    [1] https://kubernetes.io/docs/concepts/services-networking/service/#headless-services
    
    Change-Id: I85861281396402b585f1235e13b8776ee12ff87a

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/dependent/GerritAbstractService.java
@@ -0,0 +1,111 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.gerrit.dependent;
+
+import static com.google.gerrit.k8s.operator.gerrit.dependent.GerritStatefulSet.HTTP_PORT;
+import static com.google.gerrit.k8s.operator.gerrit.dependent.GerritStatefulSet.SSH_PORT;
+
+import com.google.gerrit.k8s.operator.api.model.cluster.GerritCluster;
+import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
+import com.google.gerrit.k8s.operator.gerrit.GerritReconciler;
+import com.google.gerrit.k8s.operator.util.CRUDReconcileAddKubernetesDependentResource;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.ServicePort;
+import io.fabric8.kubernetes.api.model.ServicePortBuilder;
+import io.fabric8.kubernetes.api.model.ServiceSpec;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class GerritAbstractService
+    extends CRUDReconcileAddKubernetesDependentResource<Service, Gerrit> {
+
+  public static final String HTTP_PORT_NAME = "http";
+  public static final String SSH_PORT_NAME = "ssh";
+
+  abstract ServiceSpec getSpec(Gerrit gerrit);
+
+  @Override
+  protected Service desired(Gerrit gerrit, Context<Gerrit> context) {
+    return new ServiceBuilder().withMetadata(getMetaData(gerrit)).withSpec(getSpec(gerrit)).build();
+  }
+
+  private Map<String, String> getLabels(Gerrit gerrit) {
+    return GerritCluster.getLabels(
+        gerrit.getMetadata().getName(),
+        getComponentName(gerrit),
+        GerritReconciler.class.getSimpleName());
+  }
+
+  private ObjectMeta getMetaData(Gerrit gerrit) {
+    return new ObjectMetaBuilder()
+        .withName(getName(gerrit))
+        .withNamespace(gerrit.getMetadata().getNamespace())
+        .withLabels(getLabels(gerrit))
+        .build();
+  }
+
+  String getComponentName(Gerrit gerrit) {
+    return getName(gerrit);
+  }
+
+  public GerritAbstractService() {
+    super(Service.class);
+  }
+
+  public static String getName(Gerrit gerrit) {
+    return getName(gerrit.getMetadata().getName());
+  }
+
+  public static String getName(String gerritName) {
+    return gerritName;
+  }
+
+  public static String getHostname(Gerrit gerrit) {
+    return getHostname(getName(gerrit), gerrit.getMetadata().getNamespace());
+  }
+
+  public static String getHostname(String name, String namespace) {
+    return String.format("%s.%s.svc.cluster.local", name, namespace);
+  }
+
+  public static String getUrl(Gerrit gerrit) {
+    return String.format(
+        "http://%s:%s", getHostname(gerrit), gerrit.getSpec().getService().getHttpPort());
+  }
+
+  public static List<ServicePort> getServicePorts(Gerrit gerrit) {
+    List<ServicePort> ports = new ArrayList<>();
+    ports.add(
+        new ServicePortBuilder()
+            .withName(HTTP_PORT_NAME)
+            .withPort(gerrit.getSpec().getService().getHttpPort())
+            .withNewTargetPort(HTTP_PORT)
+            .build());
+    if (gerrit.isSshEnabled()) {
+      ports.add(
+          new ServicePortBuilder()
+              .withName(SSH_PORT_NAME)
+              .withPort(gerrit.getSpec().getService().getSshPort())
+              .withNewTargetPort(SSH_PORT)
+              .build());
+    }
+    return ports;
+  }
+}
