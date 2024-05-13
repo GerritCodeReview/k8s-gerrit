2276560a1b261b856020c8461fbec886f0043702
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2024-05-13 16:06:10 +0100

    Add GerritCluster/Gerrit CR webhook validation for Multi-site setup
    This changes introduces validation for Multi-site setup in the webhook
    when GerritCluster or Gerrit CR are applied in the cluster.
    
    For GerritCluster CR the validation checks that there is only one Gerrit
    configuration and it must be Primary.
    
    For Gerrit CR the validation checks that refdb configuration must be
    provided.
    
    Change-Id: I930dd5cf749180cc6de6b81b7a5102d21becc4fd

--- operator/src/main/java/com/google/gerrit/k8s/operator/admission/servlet/GerritAdmissionWebhook.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/admission/servlet/GerritAdmissionWebhook.java
@@ -14,9 +14,9 @@
 
 package com.google.gerrit.k8s.operator.admission.servlet;
 
-import static com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig.RefDatabase.SPANNER;
-import static com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig.RefDatabase.ZOOKEEPER;
+import static com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig.RefDatabase.*;
 
+import com.google.gerrit.k8s.operator.OperatorContext;
 import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
 import com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig;
 import com.google.gerrit.k8s.operator.gerrit.config.GerritConfigBuilder;
@@ -53,7 +53,8 @@ public class GerritAdmissionWebhook extends ValidatingAdmissionWebhookServlet {
           .build();
     }
 
-    if (noRefDbConfiguredForHA(gerrit)) {
+    if ((OperatorContext.isMultisite() || gerrit.getSpec().isHighlyAvailablePrimary())
+        && !isRefdbConfigured(gerrit)) {
       return new StatusBuilder()
           .withCode(HttpServletResponse.SC_BAD_REQUEST)
           .withMessage(
@@ -87,9 +88,8 @@ public class GerritAdmissionWebhook extends ValidatingAdmissionWebhookServlet {
     new GerritConfigBuilder(gerrit).validate();
   }
 
-  private boolean noRefDbConfiguredForHA(Gerrit gerrit) {
-    return gerrit.getSpec().isHighlyAvailablePrimary()
-        && gerrit.getSpec().getRefdb().getDatabase().equals(GlobalRefDbConfig.RefDatabase.NONE);
+  private boolean isRefdbConfigured(Gerrit gerrit) {
+    return !gerrit.getSpec().getRefdb().getDatabase().equals(NONE);
   }
 
   private boolean missingRefdbConfig(Gerrit gerrit) {
