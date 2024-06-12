57181d497b4c0aa8880f258f136f5cf5fbe7b0c1
Thomas Dräbing <thomas.draebing@sap.com>
2024-06-12 10:00:36 +0200

    [Operator] Add CustomResource to create GerritIndexer job
    By default the indexes used by Gerrit will be updated using online
    reindexing. On large sites this might be quite costly and affect
    performance in the primary Gerrit instances serving clients.
    
    To avoid performance issues, this change adds a new CustomResource that
    manages a separate Gerrit instance running as a job and not exposed
    to clients that will run offline reindexing on a copy of the repositories.
    Afterwards it will copy the resulting indexes to a given output volume
    which in the followup can be mounted to the Gerrit instances to apply
    the new index versions. These copied indexes can then be used as a base
    for online reindexing with the option to reuse existing documents, allowing
    to only reindex data that was updated in the meantime.
    
    Change-Id: I1c1476bab82b992b259e3e545763e0a43759ac31

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/indexer/GerritIndexerReconciler.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.indexer;
+
+import com.google.gerrit.k8s.operator.api.model.indexer.GerritIndexer;
+import com.google.gerrit.k8s.operator.indexer.dependent.GerritIndexerConfigMap;
+import com.google.gerrit.k8s.operator.indexer.dependent.GerritIndexerJob;
+import com.google.inject.Singleton;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+
+@Singleton
+@ControllerConfiguration(
+    dependents = {
+      @Dependent(name = "gerrit-indexer-configmap", type = GerritIndexerConfigMap.class),
+      @Dependent(
+          name = "gerrit-indexer-job",
+          type = GerritIndexerJob.class,
+          dependsOn = "gerrit-indexer-configmap")
+    })
+public class GerritIndexerReconciler implements Reconciler<GerritIndexer> {
+
+  @Override
+  public UpdateControl<GerritIndexer> reconcile(
+      GerritIndexer gerritIndexer, Context<GerritIndexer> context) {
+    return UpdateControl.noUpdate();
+  }
+}
