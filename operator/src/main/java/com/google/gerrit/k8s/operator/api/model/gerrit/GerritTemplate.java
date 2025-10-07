00f5aca018e38803693d88820a4282678717b67c
Fabio Ponciroli <ponch78@gmail.com>
2025-10-07 13:06:29 +0200

    Remove Zookeeper dependency
    Change-Id: Ia1f08a1387eb64e069c1218127491225321e1801

--- operator/src/main/java/com/google/gerrit/k8s/operator/api/model/gerrit/GerritTemplate.java
+++ operator/src/main/java/com/google/gerrit/k8s/operator/api/model/gerrit/GerritTemplate.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.gerrit.k8s.operator.api.model.cluster.GerritCluster;
 import com.google.gerrit.k8s.operator.api.model.gerrit.GerritTemplateSpec.GerritMode;
-import com.google.gerrit.k8s.operator.api.model.shared.GlobalRefDbConfig;
 import com.google.gerrit.k8s.operator.api.model.shared.IngressConfig;
 import io.fabric8.kubernetes.api.model.KubernetesResource;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
@@ -83,15 +82,6 @@ public class GerritTemplate implements KubernetesResource {
 
     if (gerritCluster.getSpec().getGerrits().stream()
         .anyMatch(g -> g.getSpec().getMode().equals(GerritMode.PRIMARY))) {
-      GlobalRefDbConfig refdb = gerritCluster.getSpec().getRefdb();
-      if (refdb.getZookeeper() != null && refdb.getZookeeper().getRootNode() == null) {
-        refdb
-            .getZookeeper()
-            .setRootNode(
-                gerritCluster.getMetadata().getNamespace()
-                    + "/"
-                    + gerritCluster.getMetadata().getName());
-      }
       gerritSpec.setRefdb(gerritCluster.getSpec().getRefdb());
     }
 
