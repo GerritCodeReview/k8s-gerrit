b51f85eb4c8970250185e1e647d8ca379a95eabc
I547210 <hitesh.chaudhari@sap.com>
2025-10-30 14:47:31 +0100

    Add aws-dynamodb-refdb plugin as global-refdb implementation to the Operator
    Release-Notes: Add aws-dynamodb-refdb plugin as global-refdb implementation to the Operator
    Change-Id: Ic98a64000102a23331a67afd2482e859ee8ad49b

---
+++ operator/src/main/java/com/google/gerrit/k8s/operator/gerrit/config/DyanmoDbRefDbPluginConfigBuilder.java
@@ -0,0 +1,69 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.k8s.operator.gerrit.config;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.k8s.operator.api.model.gerrit.Gerrit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DyanmoDbRefDbPluginConfigBuilder extends ConfigBuilder {
+  public DyanmoDbRefDbPluginConfigBuilder(Gerrit gerrit) {
+    super(
+        gerrit.getSpec().getConfigFiles().getOrDefault("aws-dynamodb-refdb.config", ""),
+        ImmutableList.copyOf(collectRequiredOptions(gerrit)));
+  }
+
+  private static List<RequiredOption<?>> collectRequiredOptions(Gerrit gerrit) {
+    List<RequiredOption<?>> requiredOptions = new ArrayList<>();
+
+    requiredOptions.add(
+        new RequiredOption<String>(
+            "ref-database",
+            "aws-dynamodb-refdb",
+            "region",
+            gerrit.getSpec().getRefdb().getDynamoDb().getRegion()));
+
+    requiredOptions.add(
+        new RequiredOption<String>(
+            "ref-database",
+            "aws-dynamodb-refdb",
+            "endpoint",
+            gerrit.getSpec().getRefdb().getDynamoDb().getEndpoint()));
+
+    requiredOptions.add(
+        new RequiredOption<String>(
+            "ref-database",
+            "aws-dynamodb-refdb",
+            "locksTableName",
+            gerrit.getSpec().getRefdb().getDynamoDb().getLocksTableName()));
+
+    requiredOptions.add(
+        new RequiredOption<String>(
+            "ref-database",
+            "aws-dynamodb-refdb",
+            "refsDbTableName",
+            gerrit.getSpec().getRefdb().getDynamoDb().getRefsDbTableName()));
+
+    requiredOptions.add(
+        new RequiredOption<String>(
+            "ref-database",
+            "aws-dynamodb-refdb",
+            "profileName",
+            gerrit.getSpec().getRefdb().getDynamoDb().getProfileName()));
+
+    return requiredOptions;
+  }
+}
