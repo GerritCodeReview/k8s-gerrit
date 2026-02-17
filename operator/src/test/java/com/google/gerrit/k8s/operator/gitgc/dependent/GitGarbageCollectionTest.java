f4491bac5d7f7d1312b798d3e42e887cecba4765
Thomas Dräbing <thomas.draebing@sap.com>
2026-02-17 16:10:00 +0100

    Update JOSDK to 5.2.3
    This Update includes fixes to handle the following breaking change:
    
    - ResourceIDMapper for External Resources [1]
    
    [1]
    https://javaoperatorsdk.io/blog/2025/11/25/version-5.2-released/#resourceidmapper-for-external-resources
    
    Change-Id: I2ab87ecf2d25b05ff3f41fbc46ef278b5c923bd3

--- operator/src/test/java/com/google/gerrit/k8s/operator/gitgc/dependent/GitGarbageCollectionTest.java
+++ operator/src/test/java/com/google/gerrit/k8s/operator/gitgc/dependent/GitGarbageCollectionTest.java
@@ -81,7 +81,7 @@ public class GitGarbageCollectionTest {
             kubernetesServer.createClient());
 
     return new DefaultContext<GitGarbageCollection>(
-        new GenericRetryExecution(new GenericRetry()), controller, primary);
+        new GenericRetryExecution(new GenericRetry()), controller, primary, false, false);
   }
 
   private static Stream<Arguments> provideYamlManifests() {
